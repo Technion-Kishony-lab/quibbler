f0d04d40efbe37f718032125e8baaecd8cc12673
Maor Kleinberger <kmaork@gmail.com>
2021-09-14 19:01:08 +0300

    Made scans shallow by default

--- pyquibbler/quib/utils.py
+++ pyquibbler/quib/utils.py
@@ -1,4 +1,31 @@
+from __future__ import annotations
+from dataclasses import dataclass
 from itertools import chain
+from typing import Any, Optional, Set, TYPE_CHECKING
+from copy import copy
+
+from pyquibbler.env import is_debug
+from pyquibbler.exceptions import DebugException
+
+if TYPE_CHECKING:
+    from pyquibbler.quib import Quib
+
+SHALLOW_MAX_DEPTH = 1
+SHALLOW_MAX_LENGTH = 100
+
+
+@dataclass
+class NestedQuibException(DebugException):
+    obj: Any
+    nested_quibs: Set[Quib]
+
+    def __str__(self):
+        return 'PyQuibbler does not support calling functions with arguments that contain nested quibs.\n' \
+               f'The quibs {self.nested_quibs} are nested within {self.obj}.'
+
+    @classmethod
+    def create_from_object(cls, obj: Any):
+        return cls(obj, set(iter_quibs_in_object_recursively(obj)))
 
 
 def is_iterator_empty(iterator):
@@ -13,26 +40,54 @@ def is_iterator_empty(iterator):
         return True
 
 
-def deep_copy_and_replace_quibs_with_vals(obj):
+def deep_copy_and_replace_quibs_with_vals(obj: Any, max_depth: Optional[int] = None, max_length: Optional[int] = None):
     """
-    Returns an iterator for all quib objects nested in the given python object.
+    Deep copy an object while replacing quibs with their values.
+    When `max_depth` is given, limits the depth in which quibs are looked for.
+    `max_depth=0` means only `obj` itself will be checked and replaced,
+    `max_depth=1` means `obj` and all objects it directly references, and so on.
+    When `max_length` is given, does not recurse into iterables larger than `max_length`.
     """
     from pyquibbler.quib import Quib
     if isinstance(obj, Quib):
         return obj.get_value()
-    elif isinstance(obj, (tuple, list, set)):
-        return type(obj)(deep_copy_and_replace_quibs_with_vals(sub_obj) for sub_obj in obj)
-    elif isinstance(obj, slice):
-        return slice(deep_copy_and_replace_quibs_with_vals(obj.start),
-                     deep_copy_and_replace_quibs_with_vals(obj.stop),
-                     deep_copy_and_replace_quibs_with_vals(obj.step))
-    return obj
+    if max_depth is None or max_depth > 0:
+        # Recurse into composite objects
+        next_max_depth = None if max_depth is None else max_depth - 1
+        if isinstance(obj, (tuple, list, set)):
+            if max_length is None or len(obj) <= max_length:
+                return type(obj)(deep_copy_and_replace_quibs_with_vals(sub_obj, next_max_depth) for sub_obj in obj)
+        elif isinstance(obj, slice):
+            return slice(deep_copy_and_replace_quibs_with_vals(obj.start, next_max_depth),
+                         deep_copy_and_replace_quibs_with_vals(obj.stop, next_max_depth),
+                         deep_copy_and_replace_quibs_with_vals(obj.step, next_max_depth))
+    return copy(obj)
 
 
-def iter_quibs_in_object(obj):
+def shallow_copy_and_replace_quibs_with_vals(obj: Any):
     """
-    Returns an iterator for all quib objects nested in the given python object.
+    Deep copy `obj` while replacing quibs with their values, with a limited depth and length.
+    """
+    return deep_copy_and_replace_quibs_with_vals(obj, SHALLOW_MAX_DEPTH, SHALLOW_MAX_LENGTH)
+
+
+def copy_and_replace_quibs_with_vals(obj: Any):
+    result = shallow_copy_and_replace_quibs_with_vals(obj)
+    if is_debug():
+        expected = deep_copy_and_replace_quibs_with_vals(obj)
+        if expected != result:
+            raise NestedQuibException.create_from_object(obj)
+    return result
+
+
+def iter_quibs_in_object_recursively(obj, max_depth: Optional[int] = None, max_length: Optional[int] = None):
+    """
+    Returns an iterator for quib objects nested in the given python object.
     Quibs that appear more than once will not be repeated.
+    When `max_depth` is given, limits the depth in which quibs are looked for.
+    `max_depth=0` means only `obj` itself will be checked and replaced,
+    `max_depth=1` means `obj` and all objects it directly references, and so on.
+    When `max_length` is given, does not recurse into iterables larger than `max_length`.
     """
     from pyquibbler.quib import Quib
     quibs = set()
@@ -40,24 +95,49 @@ def iter_quibs_in_object(obj):
         if obj not in quibs:
             quibs.add(obj)
             yield obj
-    elif isinstance(obj, (tuple, list, set)):
-        for sub_obj in obj:
-            yield from iter_quibs_in_object(sub_obj)
+    elif max_depth is None or max_depth > 0:
+        # Recurse into composite objects
+        if isinstance(obj, (tuple, list, set)):
+            # This is a fixed-size collection
+            if max_length is None or len(obj) <= max_length:
+                # The collection is small enough
+                next_max_depth = None if max_depth is None else max_depth - 1
+                for sub_obj in obj:
+                    yield from iter_quibs_in_object_recursively(sub_obj, next_max_depth, max_length)
+
+
+def iter_quibs_in_object_shallowly(obj: Any):
+    """
+    Returns an iterator for quib objects nested within the given python object,
+    with limited width and length while scanning for quibs.
+    """
+    return iter_quibs_in_object_recursively(obj, SHALLOW_MAX_DEPTH, SHALLOW_MAX_LENGTH)
+
+
+def iter_quibs_in_object(obj: Any):
+    result = iter_quibs_in_object_shallowly(obj)
+    if is_debug():
+        collected_result = set(result)
+        result = iter(collected_result)
+        expected = set(iter_quibs_in_object_recursively(obj))
+        if collected_result != expected:
+            raise NestedQuibException(obj, set(expected))
+    return result
 
 
 def iter_quibs_in_args(args, kwargs):
     """
     Returns an iterator for all quib objects nested in the given args and kwargs.
     """
-    return iter_quibs_in_object(list(chain(args, kwargs.values())))
+    return chain(*map(iter_quibs_in_object, chain(args, kwargs.values())))
 
 
 def call_func_with_quib_values(func, args, kwargs):
     """
     Calls a function with the specified args and kwargs while replacing quibs with their values.
     """
-    new_args = [deep_copy_and_replace_quibs_with_vals(arg) for arg in args]
-    kwargs = {name: deep_copy_and_replace_quibs_with_vals(val) for name, val in kwargs.items()}
+    new_args = [copy_and_replace_quibs_with_vals(arg) for arg in args]
+    kwargs = {name: copy_and_replace_quibs_with_vals(val) for name, val in kwargs.items()}
     return func(*new_args, **kwargs)
 
 
--- tests/quib/test_utils.py
+++ tests/quib/test_utils.py
@@ -1,11 +1,10 @@
 from operator import add
-
-from pytest import mark
+from pytest import mark, raises
 
 from pyquibbler import iquib
-from pyquibbler.quib.utils import is_iterator_empty, deep_copy_and_replace_quibs_with_vals, iter_quibs_in_object, \
-    call_func_with_quib_values, iter_quibs_in_args, \
-    is_there_a_quib_in_args
+from pyquibbler.quib.utils import is_iterator_empty, deep_copy_and_replace_quibs_with_vals, \
+    iter_quibs_in_object_recursively, call_func_with_quib_values, iter_quibs_in_args, \
+    is_there_a_quib_in_args, NestedQuibException, copy_and_replace_quibs_with_vals, iter_quibs_in_object
 
 iquib1 = iquib(1)
 iquib2 = iquib(2)
@@ -19,32 +18,67 @@ def test_is_iterator_empty(iterator, expected_result):
     assert is_iterator_empty(iterator) == expected_result
 
 
-@mark.parametrize(['to_copy', 'expected_result'], [
-    ([1, [2]], [1, [2]]),
-    (iquib1, 1),
-    ([iquib1], [1]),
-    ([1, [iquib1, [iquib2]]], [1, [1, [2]]]),
+@mark.parametrize(['to_copy', 'depth', 'length', 'expected_result'], [
+    ([1, [2]], None, None, [1, [2]]),
+    ([1, [2]], 0, 0, [1, [2]]),
+    ([1, [2]], 1, 10, [1, [2]]),
+    ([1, [2]], 5, 10, [1, [2]]),
+    (iquib1, None, None, 1),
+    (iquib1, 0, 0, 1),
+    (iquib1, 3, 3, 1),
+    ([iquib1], None, None, [1]),
+    ([iquib1], 0, None, [iquib1]),
+    ([iquib1], 1, None, [1]),
+    ([iquib1], 2, None, [1]),
+    ([iquib1], None, 0, [iquib1]),
+    ([iquib1], None, 1, [1]),
+    ([1, [iquib1, [iquib2]]], None, None, [1, [1, [2]]]),
+    ([1, [iquib1, [iquib2]]], 0, None, [1, [iquib1, [iquib2]]]),
+    ([1, [iquib1, [iquib2]]], 1, None, [1, [iquib1, [iquib2]]]),
+    ([1, [iquib1, [iquib2]]], 2, None, [1, [1, [iquib2]]]),
+    ([1, [iquib1, [iquib2]]], 3, None, [1, [1, [2]]]),
+    ([1, [iquib1, [iquib2]]], None, 0, [1, [iquib1, [iquib2]]]),
+    ([1, [iquib1, [iquib2]]], None, 1, [1, [iquib1, [iquib2]]]),
+    ([1, [iquib1, [iquib2]]], None, 2, [1, [1, [2]]]),
 ])
-def test_deep_copy_and_replace_quibs_with_vals(to_copy, expected_result):
-    assert deep_copy_and_replace_quibs_with_vals(to_copy) == expected_result
+def test_deep_copy_and_replace_quibs_with_vals(to_copy, depth, length, expected_result):
+    assert deep_copy_and_replace_quibs_with_vals(to_copy, depth, length) == expected_result
 
 
-@mark.parametrize(['to_iter', 'expected_result'], [
-    ([1, [2]], set()),
-    (None, set()),
-    (iquib1, {iquib1}),
-    ([1, [iquib1, iquib2], iquib1], {iquib1, iquib2}),
+@mark.parametrize(['to_iter', 'depth', 'length', 'expected_result'], [
+    ([1, [2]], None, None, set()),
+    ([1, [2]], 0, 0, set()),
+    ([1, [2]], 0, 1, set()),
+    ([1, [2]], 0, 2, set()),
+    ([1, [2]], 10, 0, set()),
+    (iquib1, None, None, {iquib1}),
+    (iquib1, 0, 0, {iquib1}),
+    (iquib1, 3, 3, {iquib1}),
+    ([iquib1], None, None, {iquib1}),
+    ([iquib1, iquib1, iquib2], None, None, {iquib1, iquib2}),
+    ([iquib1], 0, None, set()),
+    ([iquib1], 1, None, {iquib1}),
+    ([iquib1], 2, None, {iquib1}),
+    ([iquib1], None, 0, set()),
+    ([iquib1], None, 1, {iquib1}),
+    ([1, [iquib1, [iquib2]]], None, None, {iquib1, iquib2}),
+    ([1, [iquib1, [iquib2]]], 0, None, set()),
+    ([1, [iquib1, [iquib2]]], 1, None, set()),
+    ([1, [iquib1, [iquib2]]], 2, None, {iquib1}),
+    ([1, [iquib1, [iquib2]]], 3, None, {iquib1, iquib2}),
+    ([1, [iquib1, [iquib2]]], None, 0, set()),
+    ([1, [iquib1, [iquib2]]], None, 1, set()),
+    ([1, [iquib1, [iquib2]]], None, 2, {iquib1, iquib2}),
 ])
-def test_iter_quibs_in_object(to_iter, expected_result):
-    assert set(iter_quibs_in_object(to_iter)) == expected_result
+def test_iter_quibs_in_object(to_iter, depth, length, expected_result):
+    assert set(iter_quibs_in_object_recursively(to_iter, depth, length)) == expected_result
 
 
 @mark.parametrize(['func', 'args', 'kwargs', 'result'], [
     (add, (1, 1), dict(), 2),
-    (add, (1, iquib(1)), dict(), 2),
-    (add, (iquib(1), iquib(1)), dict(), 2),
-    (lambda x, y: x + y[0], (1, [iquib(1)]), dict(), 2),
-    (lambda x, y: x + y[0][0], (1, [[iquib(1)]]), dict(), 2),
+    (add, (1, iquib1), dict(), 2),
+    (add, (iquib1, iquib1), dict(), 2),
+    (lambda x, y: x + y[0], (1, [iquib1]), dict(), 2),
     (lambda x, y: x + y, (1,), dict(y=1), 2),
 ])
 def test_call_func_with_quib_values(func, args, kwargs, result):
@@ -56,7 +90,7 @@ args_kwargs_quibs_test = mark.parametrize(['args', 'kwargs', 'quibs'], [
     ((1,), dict(a=1), set()),
     ((iquib1,), dict(a=1), {iquib1}),
     ((iquib1,), dict(a=iquib2), {iquib1, iquib2}),
-    ((), dict(a=[(0, iquib2)]), {iquib2}),
+    ((), dict(a=[iquib2]), {iquib2}),
 ])
 
 
@@ -68,3 +102,22 @@ def test_iter_quibs_in_args(args, kwargs, quibs):
 @args_kwargs_quibs_test
 def test_is_there_a_quib_in_args(args, kwargs, quibs):
     assert is_there_a_quib_in_args(args, kwargs) == (len(quibs) > 0)
+
+
+def test_copy_and_replace_quibs_with_vals_raises_when_receives_nested_quibs():
+    obj = [[iquib1]]
+    with raises(NestedQuibException) as exc_info:
+        copy_and_replace_quibs_with_vals(obj)
+
+    assert exc_info.type is NestedQuibException
+    assert exc_info.value.obj is obj
+    assert exc_info.value.nested_quibs == {iquib1}
+
+def test_iter_quibs_in_object_raises_when_receives_nested_quibs():
+    obj = [[iquib1]]
+    with raises(NestedQuibException) as exc_info:
+        iter_quibs_in_object(obj)
+
+    assert exc_info.type is NestedQuibException
+    assert exc_info.value.obj is obj
+    assert exc_info.value.nested_quibs == {iquib1}
