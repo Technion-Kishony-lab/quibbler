efd4d982ae82e866e566b4a3bab3b632944f6b49
Maor Kleinberger <kmaork@gmail.com>
2021-09-26 17:19:14 +0300

    Added assignment template

---
+++ pyquibbler/quib/assignment_template.py
@@ -0,0 +1,85 @@
+from abc import ABC, abstractmethod
+from math import floor
+from typing import Callable, Any
+from dataclasses import dataclass
+
+from pyquibbler.exceptions import DebugException
+
+
+@dataclass
+class BoundMaxBelowMinException(DebugException):
+    minimum: Any
+    maximum: Any
+
+    def __str__(self):
+        return f'Maximum ({self.maximum} is smaller than minimum ({self.minimum})'
+
+
+@dataclass
+class RangeStopBelowStartException(DebugException):
+    start: Any
+    stop: Any
+
+    def __str__(self):
+        return f'Stop ({self.stop} is smaller than start ({self.start})'
+
+
+def get_number_in_bounds(number, minimum, maximum):
+    return min(max(number, minimum), maximum)
+
+
+@dataclass
+class AssignmentTemplate(ABC):
+    @abstractmethod
+    def convert(self, data):
+        """
+        Convert the given object to match the template.
+        """
+
+
+@dataclass
+class CastingAssignmentTemplate(AssignmentTemplate):
+    """
+    Converts assigned data to a given type.
+    """
+
+    type: Callable
+
+    def convert(self, data):
+        return self.type(data)
+
+
+@dataclass
+class BoundAssinmentTemplate(AssignmentTemplate):
+    """
+    Limits assigned data to specific minimum and maximum bounds.
+    """
+    minimum: Any
+    maximum: Any
+
+    def __post_init__(self):
+        if self.maximum < self.minimum:
+            raise BoundMaxBelowMinException(self.minimum, self.maximum)
+
+    def convert(self, data):
+        return get_number_in_bounds(data, self.minimum, self.maximum)
+
+
+@dataclass
+class RangeAssignmentTemplate(AssignmentTemplate):
+    """
+    Limits assigned data to a given range.
+    """
+
+    start: Any
+    stop: Any
+    step: Any
+
+    def __post_init__(self):
+        if self.stop < self.start:
+            raise RangeStopBelowStartException(self.start, self.stop)
+
+    def convert(self, data):
+        rounded = round((data - self.start) / self.step)
+        bound = get_number_in_bounds(rounded, 0, floor((self.stop - self.start) / self.step))
+        return type(data)(self.start + bound * self.step)
---
+++ tests/unit/quib/test_assignment_template.py
@@ -0,0 +1,82 @@
+from pytest import mark, raises
+
+from pyquibbler.quib.assignment_template import CastingAssignmentTemplate, BoundAssinmentTemplate, \
+    RangeAssignmentTemplate, BoundMaxBelowMinException, RangeStopBelowStartException
+
+
+@mark.parametrize(['template', 'data', 'expected'], [
+    # Casting
+    (CastingAssignmentTemplate(int), 3.14, 3),
+    (CastingAssignmentTemplate(float), 3, 3.),
+
+    # Bound int
+    (BoundAssinmentTemplate(0, 2), -1, 0),
+    (BoundAssinmentTemplate(0, 2), 0, 0),
+    (BoundAssinmentTemplate(0, 2), 1, 1),
+    (BoundAssinmentTemplate(0, 2), 2, 2),
+    (BoundAssinmentTemplate(0, 2), 3, 2),
+    (BoundAssinmentTemplate(0, 2), 3, 2),
+
+    # Bound float
+    (BoundAssinmentTemplate(0.1, 0.2), 0., 0.1),
+    (BoundAssinmentTemplate(0.1, 0.2), 0.15, 0.15),
+    (BoundAssinmentTemplate(0.1, 0.2), 0.2, 0.2),
+
+    # Range int
+    (RangeAssignmentTemplate(-3, 3, 3), -7, -3),
+    (RangeAssignmentTemplate(-3, 3, 3), -4, -3),
+    (RangeAssignmentTemplate(-3, 3, 3), -3, -3),
+    (RangeAssignmentTemplate(-3, 3, 3), -2, -3),
+    (RangeAssignmentTemplate(-3, 3, 3), -1, 0),
+    (RangeAssignmentTemplate(-3, 3, 3), 0, 0),
+    (RangeAssignmentTemplate(-3, 3, 3), 1, 0),
+    (RangeAssignmentTemplate(-3, 3, 3), 2, 3),
+    (RangeAssignmentTemplate(-3, 3, 3), 6, 3),
+    (RangeAssignmentTemplate(-3, 3, 3), 10, 3),
+
+    # Range when stop is not divisible by step
+    (RangeAssignmentTemplate(0, 10, 4), 7, 8),
+    (RangeAssignmentTemplate(0, 10, 4), 8, 8),
+    (RangeAssignmentTemplate(0, 10, 4), 9, 8),
+    (RangeAssignmentTemplate(0, 10, 4), 10, 8),
+    (RangeAssignmentTemplate(0, 10, 4), 11, 8),
+    (RangeAssignmentTemplate(0, 10, 4), 12, 8),
+    (RangeAssignmentTemplate(0, 10, 4), 13, 8),
+
+    # Range when step > stop - start
+    (RangeAssignmentTemplate(1, 10, 15), -110, 1),
+    (RangeAssignmentTemplate(1, 10, 15), -5, 1),
+    (RangeAssignmentTemplate(1, 10, 15), 1, 1),
+    (RangeAssignmentTemplate(1, 10, 15), 9, 1),
+    (RangeAssignmentTemplate(1, 10, 15), 10, 1),
+    (RangeAssignmentTemplate(1, 10, 15), 15, 1),
+    (RangeAssignmentTemplate(1, 10, 15), 110, 1),
+    (RangeAssignmentTemplate(1, 1, 15), 110, 1),
+
+    # Range float
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), -0.7, -0.3),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), -0.4, -0.3),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), -0.3, -0.3),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), -0.2, -0.3),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), -0.1, 0.0),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), 0.0, 0.),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), 0.1, 0.),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), 0.2, 0.3),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), 0.6, 0.3),
+    (RangeAssignmentTemplate(-0.3, 0.4, 0.3), 1., 0.3),
+    (RangeAssignmentTemplate(-0.3, -0.3, 0.3), 1., -0.3),
+])
+def test_casting_assignment_template(template, data, expected):
+    result = template.convert(data)
+    assert result == expected
+    assert type(result) is type(expected)
+
+
+def test_cant_create_bound_assignment_template_with_max_smaller_than_min():
+    with raises(BoundMaxBelowMinException):
+        BoundAssinmentTemplate(1., 0.9)
+
+
+def test_cant_create_range_assignment_template_with_stop_smaller_than_start():
+    with raises(RangeStopBelowStartException):
+        RangeAssignmentTemplate(1., 0.9, 3.)
