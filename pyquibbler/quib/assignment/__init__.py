8de8ca650a17e03cca970a440376e03666cab90e
Maor Kleinberger <kmaork@gmail.com>
2021-10-06 19:06:27 +0300

    added repr for ReplaceObject

--- pyquibbler/quib/assignment/__init__.py
+++ pyquibbler/quib/assignment/__init__.py
@@ -1,5 +1,5 @@
 from .overrider import Overrider
-from .assignment import Assignment, QuibWithAssignment, ReplaceObject
+from .assignment import Assignment, QuibWithAssignment
 from .assignment_template import AssignmentTemplate, RangeAssignmentTemplate, BoundAssignmentTemplate, \
     BoundMaxBelowMinException, RangeStopBelowStartException
 from .reverse_assignment import reverse_function_quib, CannotReverseUnknownFunctionException
