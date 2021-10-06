9ab4680f3492c686cb7ce456fed9051f6e8543ce
Maor Kleinberger <kmaork@gmail.com>
2021-10-06 18:11:50 +0300

    Small fixes

--- examples/dialog_example.py
+++ examples/dialog_example.py
@@ -23,7 +23,9 @@ c.allow_overriding = True
 d = c + 1
 d.allow_overriding = True
 
-# everything = np.concatenate((q(list, a), q(list, a2), q(list, b), q(list, b2), c, d))
-# print(everything.get_value())
-plt.plot(d, marker='o', markersize=18, picker=True, pickradius=20)
+allz = (c, d)  # (q(np.array, a), q(np.array, a2), q(np.array, b), q(np.array, b2), c, d)
+print([a.get_value() for a in allz])
+everything = np.concatenate(allz)
+print(everything.get_value())
+plt.plot(everything, marker='o', markersize=18, picker=True, pickradius=20)
 plt.show()
