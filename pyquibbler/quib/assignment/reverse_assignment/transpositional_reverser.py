9ba13ae4723e7529780da8b772a4bc96d01bd800
Maor <maor.kern@gmail.com>
2021-10-07 14:55:28 +0300

    Add comment

--- pyquibbler/quib/assignment/reverse_assignment/transpositional_reverser.py
+++ pyquibbler/quib/assignment/reverse_assignment/transpositional_reverser.py
@@ -38,6 +38,14 @@ class TranspositionalReverser(Reverser):
 
     @functools.lru_cache()
     def _get_quibs_which_can_change(self):
+        """
+        Return a list of quibs that can potentially change as a result of the transpositional function- this does NOT
+        necessarily mean these quibs will in fact be changed.
+
+        For example, in `np.repeat(q1, q2)`, where q1 is a numpy array quib
+        and q2 is a number quib with amount of times to repeat, q2 cannot in any
+        situation be changed by a change in `np.repeat`'s result. So only `q1` would be returned.
+        """
         from pyquibbler.quib import Quib
         potentially_changed_quib_indices = self.SUPPORTED_FUNCTIONS_TO_POTENTIALLY_CHANGED_QUIB_INDICES[self._func]
         quibs = []
