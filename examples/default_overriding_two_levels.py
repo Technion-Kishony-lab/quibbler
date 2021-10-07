d765e00638ffe57c0e67a28cb5f905ce185375f9
Maor Kleinberger <kmaork@gmail.com>
2021-10-07 17:07:52 +0300

    Dialog demo works

--- examples/default_overriding_two_levels.py
+++ examples/default_overriding_two_levels.py
@@ -19,17 +19,12 @@ plt.subplot(2, 1, 1)
 x = np.arange(n_plates)
 plt.bar(x, plate_factor, color=(0.7, 0.7, 0.7))
 plt.plot([-0.5, n_plates - 0.5], default_factor[[0, 0]], linewidth=5, picker=True)
-ax1 = plt.gca()
 
-
-def draw_plate_marker(i):
-    color = q(tuple, [(0, 1, 0), (1, 0, 0)])[plate_factor.get_override_mask()[i[0]]]
-    ax1.plot(x[i], plate_factor[i],
+for i in range(len(x)):
+    color = q(tuple, [(0, 1, 0), (1, 0, 0)])[plate_factor.get_override_mask()[i]]
+    plt.plot(x[[i]], plate_factor[[i]],
              marker='s', markerfacecolor=color, markersize=9,
              linestyle='None', picker=True)
-
-
-np.apply_along_axis(draw_plate_marker, 0, np.reshape(np.arange(q(len, x)), (q(len, x), 1)))
 
 plt.axis([-0.5, n_plates - 0.5, 0, 100])
 plt.ylabel('Plate factor')
@@ -41,21 +36,19 @@ well_factor.allow_overriding = True
 plt.subplot(2, 1, 2)
 dd = np.linspace(-0.4, 0.4, n_wells + 1)
 dd = (dd[0:-1] + dd[1:]) / 2.
-xx = np.reshape(x + np.reshape(dd, (n_wells, 1)), (n_wells * n_plates,))
+xx = np.ravel(x + np.reshape(dd, (n_wells, 1)), 'F')
 plt.bar(xx, well_factor, color=(0.7, 0.7, 0.7), width=0.1)
 plt.axis([-0.5, n_plates - 0.5, 0, 100])
 
 ax2 = plt.gca()
 
-
-def draw_well_marker(i):
-    color = q(tuple, [(0, 1, 0), (1, 0, 0)])[well_factor.get_override_mask()[i[0]]]
-    ax2.plot(xx[i], well_factor[i],
+for i in range(len(xx)):
+    color = q(tuple, [(0, 1, 0), (1, 0, 0)])[well_factor.get_override_mask()[i]]
+    plt.plot(xx[[i]], well_factor[[i]],  # TODO: without list
              marker='s', markerfacecolor=color, markersize=9,
              linestyle='None', picker=True)
 
-
-np.apply_along_axis(draw_well_marker, -1, np.reshape(np.arange(q(len, xx)), (q(len, xx), 1)))
-
 plt.ylabel('Well factor')
 plt.show()
+# correct caching for level choosing when recalled
+# when override source iquib from overridden value, dragging is weird
--- pyquibbler/third_party_overriding/overriding.py
+++ pyquibbler/third_party_overriding/overriding.py
@@ -17,7 +17,8 @@ from pyquibbler.utils import ensure_only_run_once_globally
 NUMPY_OVERRIDES = [
     (np, [
         (DefaultFunctionQuib, ["abs", "average", "around", "square", "repeat", "max", "arange", "polyfit",
-                               "linspace", "polyval", "full", "concatenate", "array", "reshape", "genfromtxt"]),
+                               "linspace", "polyval", "full", "concatenate", "array", "reshape", "genfromtxt",
+                               "ravel"]),
         (GraphicsFunctionQuib, ['apply_along_axis', 'apply_over_axes']),
     ]),
     (np.random, [
