c1b2eee20caafdaf744b121a465f09a89bc7e353
Maor <maor.kern@gmail.com>
2021-10-10 21:11:44 +0300

    Change default to be graphicsfunctionquib

--- pyquibbler/user_utils.py
+++ pyquibbler/user_utils.py
@@ -1,4 +1,4 @@
-from pyquibbler.quib import DefaultFunctionQuib
+from pyquibbler.quib import GraphicsFunctionQuib
 
 
 def q(func, *args, **kwargs):
@@ -6,5 +6,5 @@ def q(func, *args, **kwargs):
     Creates a function quib from the given function call.
     """
     # In case the given function is already a wrapper for a specific quib type, we use it.
-    quib_type = getattr(func, '__quib_wrapper__', DefaultFunctionQuib)
+    quib_type = getattr(func, '__quib_wrapper__', GraphicsFunctionQuib)
     return quib_type.create(func=func, func_args=args, func_kwargs=kwargs)
