d82cd0146558e827457d410e60477d5e266ed5e2
Maor <maor.kern@gmail.com>
2021-10-11 23:31:04 +0300

    Remove unneeded consts file

--- pyquibbler/quib/assignment/consts.py
+++
@@ -1,9 +0,0 @@
-
-SUPPORTED_FUNCTIONS_TO_POTENTIALLY_CHANGED_QUIB_INDICES = {
-    np.rot90: {0},
-    np.concatenate: {0},
-    np.repeat: {0},
-    np.full: {1},
-    getitem: {0},
-    np.reshape: {0}
-}
