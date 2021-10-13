4ec3e68610c281bdc94f0c4b250212f30f316bd9
Maor <maor.kern@gmail.com>
2021-10-13 15:49:18 +0300

    Add test for elementwise function quib

---
+++ tests/unit/quib/function_quibs/test_elementwise_function_quib.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from pyquibbler import iquib
+from pyquibbler.quib.assignment.assignment import PathComponent
+
+
+def test_elementwise_function_quib_invalidation_when_should_invalidate():
+    a = iquib(np.array([1, 2, 3]))
+    b = a + 1
+    b.get_value()
+
+    a.invalidate_and_redraw_at_path(path=[PathComponent(
+        component=0,
+        indexed_cls=list
+    )])
+
+    assert not b.is_cache_valid
