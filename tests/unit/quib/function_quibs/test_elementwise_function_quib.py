5b6ee23a8efa888eb437b706a9f1464601da3107
Maor <maor.kern@gmail.com>
2021-10-13 16:39:54 +0300

    Add failing test

--- tests/unit/quib/function_quibs/test_elementwise_function_quib.py
+++ tests/unit/quib/function_quibs/test_elementwise_function_quib.py
@@ -30,3 +30,17 @@ def test_elementwise_function_quib_invalidation_when_should_not_invalidate():
     )])
 
     assert not item.is_cache_valid
+
+
+def test_elementwise_function_quib_invalidation_at_field_does_nothing():
+    dtype = [('inteliggence', np.int_), ('age', np.int_)]
+    a = iquib(np.array([(100, 24)], dtype=dtype))
+    b = a['age'] + 1
+    b.get_value()
+    
+    a.invalidate_and_redraw_at_path(path=[PathComponent(
+        component='age',
+        indexed_cls=np.ndarray
+    )])
+
+    assert not b.is_cache_valid
