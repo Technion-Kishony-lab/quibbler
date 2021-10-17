9d688b97d5e8f780e546f82086c4c6727202c471
Maor <maor.kern@gmail.com>
2021-10-17 18:34:12 +0300

    Add flag to conftest

--- pyquibbler/env.py
+++ pyquibbler/env.py
@@ -2,3 +2,5 @@ from .utils import Flag
 
 DEBUG = Flag(False)
 LAZY = Flag(True)
+ASSIGNMENT_RESTRICTIONS = Flag(False)
+
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,13 @@
 from pytest import fixture
 
 from pyquibbler import CacheBehavior, override_all
-from pyquibbler.env import DEBUG, LAZY
+from pyquibbler.env import DEBUG, LAZY, ASSIGNMENT_RESTRICTIONS
 from pyquibbler.quib import FunctionQuib
 from pyquibbler.utils import Flag
 
 DEFAULT_DEBUG = True
 DEFAULT_LAZY = True
+DEFAULT_ASSIGNMENT_RESTRICTIONS = False
 
 
 @fixture(scope="session", autouse=True)
@@ -20,6 +21,8 @@ def pytest_configure(config):
     config.addinivalue_line("markers", "debug(on): mark test to run with or without debug mode")
     config.addinivalue_line("markers", "lazy(on): mark test to run with or without lazy mode")
     config.addinivalue_line("markers", "regression: mark test as regression test")
+    config.addinivalue_line("markers", "assignment_restrictions(on): mark test to run with or without assignment "
+                                       "restrictions mode")
     # Copied from matplotlib to disable warning
     config.addinivalue_line("markers", "style: Set alternate Matplotlib style temporarily (deprecated).")
 
@@ -35,6 +38,7 @@ def parametrize_flag_fixture(metafunc, name, fixture_name):
 def pytest_generate_tests(metafunc):
     parametrize_flag_fixture(metafunc, 'debug', 'setup_debug')
     parametrize_flag_fixture(metafunc, 'lazy', 'setup_lazy')
+    parametrize_flag_fixture(metafunc, 'assignment_restrictions', 'setup_assignment_restrictions')
 
 
 def setup_flag(flag: Flag, default: bool, request):
@@ -51,3 +55,8 @@ def setup_debug(request):
 @fixture(autouse=True)
 def setup_lazy(request):
     yield from setup_flag(LAZY, DEFAULT_LAZY, request)
+
+
+@fixture(autouse=True)
+def setup_assignment_restrictions(request):
+    yield from setup_flag(ASSIGNMENT_RESTRICTIONS, DEFAULT_ASSIGNMENT_RESTRICTIONS, request)
