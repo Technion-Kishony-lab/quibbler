e0b8922f2b34d72441a467107208c62c7d22ebb2
Roy Kishony <rkishony@technion.ac.il>
2021-10-18 02:11:49 +0300

    q_graphics

--- pyquibbler/__init__.py
+++ pyquibbler/__init__.py
@@ -2,5 +2,5 @@ from .decorators import quibbler_user_function
 from .performance_utils import timer, get_timer
 from .quib import iquib, CacheBehavior
 from .third_party_overriding import override_all
-from .user_utils import q
+from .user_utils import q, q_graphics
 from .quib.assignment import Assignment
--- pyquibbler/user_utils.py
+++ pyquibbler/user_utils.py
@@ -1,3 +1,4 @@
+from pyquibbler.quib import GraphicsFunctionQuib
 from pyquibbler.quib import DefaultFunctionQuib
 
 
@@ -7,4 +8,13 @@ def q(func, *args, **kwargs):
     """
     # In case the given function is already a wrapper for a specific quib type, we use it.
     quib_type = getattr(func, '__quib_wrapper__', DefaultFunctionQuib)
+    return quib_type.create(func=func, func_args=args, func_kwargs=kwargs)
+
+
+def q_graphics(func, *args, **kwargs):
+    """
+    Creates a graphical function quib from the given function call.
+    """
+    # In case the given function is already a wrapper for a specific quib type, we use it.
+    quib_type = getattr(func, '__quib_wrapper__', GraphicsFunctionQuib)
     return quib_type.create(func=func, func_args=args, func_kwargs=kwargs)
