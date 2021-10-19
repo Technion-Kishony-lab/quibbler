36c681ddfc3c00d74d539eea1335a796c7b73bd6
Maor <maor.kern@gmail.com>
2021-10-19 12:15:50 +0300

    Change signatures

--- pyquibbler/quib/function_quibs/cache.py
+++ pyquibbler/quib/function_quibs/cache.py
@@ -8,15 +8,15 @@ class ShallowCache:
     def __init__(self, value: Any = None):
         self._value = value
 
-    def set_valid_value_at_path_component(self, path_component: PathComponent):
+    def set_valid_value_at_path_component(self, path_component: PathComponent, value: Any) -> None:
         pass
 
-    def set_invalid_at_path_component(self, path_component: PathComponent):
+    def set_invalid_at_path_component(self, path_component: PathComponent) -> None:
         pass
 
-    def is_valid_at_path_component(self, path_component: PathComponent):
+    def is_valid_at_path_component(self, path_component: PathComponent) -> bool:
         pass
 
-    def get_value_at_path_component(self, path_component: PathComponent):
+    def get_value_at_path_component(self, path_component: PathComponent) -> Any:
         pass
 
