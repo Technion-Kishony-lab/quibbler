07c6d5be6b468af823f7adc7dc58644a90e52f9c
Maor <maor.kern@gmail.com>
2021-10-19 12:08:50 +0300

    Create cache interface

---
+++ pyquibbler/quib/function_quibs/cache.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from pyquibbler.quib.assignment import PathComponent
+
+
+class ShallowCache:
+
+    def __init__(self):
+        pass
+
+    def set_valid_value_at_path_component(self, path_component: PathComponent):
+        pass
+
+    def is_valid_at_path_component(self, path_component: PathComponent):
+        pass
+
+    def get_value_at_path_component(self, path_component: PathComponent):
+        pass
+
