a2b17930fb669f625746faff0d9c6e3087dbd971
Maor Kleinberger <kmaork@gmail.com>
2021-10-20 23:30:58 +0300

    Added support for apply along axis invalidation

--- pyquibbler/quib/graphics/axiswise_function_quib.py
+++ pyquibbler/quib/graphics/axiswise_function_quib.py
@@ -20,9 +20,10 @@ When a specific index is invalidated in a data source of an axiswise function, t
 the function result can be built by taking only the indexes in the loop dimensions.
 """
 import numpy as np
-from typing import Any
+from abc import abstractmethod
+from typing import Any, Dict
 
-from pyquibbler.quib import Quib
+from pyquibbler.quib.quib import Quib
 
 from .graphics_function_quib import GraphicsFunctionQuib
 from ..assignment.inverse_assignment.utils import create_empty_array_with_values_at_indices
@@ -30,22 +31,59 @@ from ..function_quibs.indices_translator_function_quib import IndicesTranslatorF
 
 
 class AxisWiseGraphicsFunctionQuib(GraphicsFunctionQuib, IndicesTranslatorFunctionQuib):
-    SUPPORTED_FUNCTIONS = {
-        np.sum: SupportedFunction({0}),
-        np.cumsum: SupportedFunction({0}),
-        np.min: SupportedFunction({0}),
-        np.max: SupportedFunction({0}),
-    }
+    SUPPORTED_KWARGS: Dict[str, str] = {}
+    REQUIRED_KWARGS: Dict[str, str] = {}
 
-    def _forward_translate_indices_to_bool_mask(self, invalidator_quib: Quib, indices: Any) -> Any:
-        source_bools = create_empty_array_with_values_at_indices(
+    def _get_source_shaped_bool_mask(self, invalidator_quib: Quib, indices: Any) -> Any:
+        return create_empty_array_with_values_at_indices(
             value=True,
             empty_value=False,
             indices=indices,
             shape=invalidator_quib.get_shape().get_value()
         )
-        kwargs = dict(axis=self._get_all_args_dict()['axis'], dtype=np.bool)
-        for kwarg in ['keepdims', 'where']:
-            if kwarg in self._get_all_args_dict(include_defaults=False):
-                kwargs[kwarg] = self._get_all_args_dict()[kwarg]
-        return np.logical_or.reduce(source_bools, **kwargs)
+
+    @abstractmethod
+    def _call_translator_func(self, kwargs, boolean_mask, invalidator_quib: Quib):
+        pass
+
+    def _forward_translate_indices_to_bool_mask(self, invalidator_quib: Quib, indices: Any) -> Any:
+        source_bool_mask = self._get_source_shaped_bool_mask(invalidator_quib, indices)
+        kwargs = {}
+        for original_kwarg_name, translator_kwarg_name in self.SUPPORTED_KWARGS.items():
+            if original_kwarg_name in self._get_all_args_dict(include_defaults=False):
+                kwargs[translator_kwarg_name] = self._get_all_args_dict(include_defaults=False)[original_kwarg_name]
+        for original_kwarg_name, translator_kwarg_name in self.REQUIRED_KWARGS.items():
+            kwargs[translator_kwarg_name] = self._get_all_args_dict(include_defaults=True)[original_kwarg_name]
+        return self._call_translator_func(kwargs, source_bool_mask, invalidator_quib)
+
+
+class ReductionAxisWiseGraphicsFunctionQuib(AxisWiseGraphicsFunctionQuib):
+    SUPPORTED_FUNCTIONS = {
+        np.sum: SupportedFunction({0}),
+        np.min: SupportedFunction({0}),
+        np.max: SupportedFunction({0}),
+    }
+    SUPPORTED_KWARGS = {'keepdims': 'keepdims', 'where': 'where'}
+    REQUIRED_KWARGS = {'axis': 'axis'}
+
+    def _call_translator_func(self, kwargs, boolean_mask, invalidator_quib: Quib):
+        return np.logical_or.reduce(boolean_mask, **kwargs)
+
+
+class AlongAxisGraphicsFunctionQuib(AxisWiseGraphicsFunctionQuib):
+    SUPPORTED_FUNCTIONS = {
+        np.apply_along_axis: SupportedFunction({2}),
+    }
+    REQUIRED_KWARGS = {'axis': 'axis'}
+
+    def _call_translator_func(self, kwargs, boolean_mask, invalidator_quib: Quib):
+        result_shape = self.get_shape().get_value()
+        func_result_ndim = len(result_shape) - len(invalidator_quib.get_shape().get_value()) + 1
+        assert func_result_ndim >= 0, func_result_ndim
+        axis = kwargs.pop('axis')
+        applied = np.apply_along_axis(np.any, axis, boolean_mask, **kwargs)
+        dims_to_expand = range(axis, axis + func_result_ndim) if axis >= 0 else \
+            range(axis, axis - func_result_ndim, -1)
+        expanded = np.expand_dims(applied, tuple(dims_to_expand))
+        broadcast = np.broadcast_to(expanded, result_shape)
+        return broadcast
--- tests/unit/quib/graphics/test_axiswise_graphics_function_quib.py
+++ tests/unit/quib/graphics/test_axiswise_graphics_function_quib.py
@@ -4,18 +4,10 @@ from pytest import mark
 from pyquibbler import iquib
 
 
-@mark.parametrize(['axis', 'indices_to_invalidate', 'expected_invalidated_result_indices', 'kwargs'], [
-    (0, (0, 1, 2), {(1, 2,)}, dict()),
-    (1, 0, {(0, 0), (0, 1), (0, 2)}, dict()),
-    (None, 0, {()}, dict()),
-    ((0, 2), (0, 0), {(0,)}, dict()),
-    (None, 0, {(0, 0, 0)}, dict(keepdims=True)),
-    (2, (0, 1), set(), dict(where=[[[True], [False]]])),
-])
-def test_axiswise_invalidation(axis, indices_to_invalidate, expected_invalidated_result_indices, kwargs):
+def check_invalidation(func, kwargs, data_kwarg, indices_to_invalidate):
     arr = np.array([[[1, 2, 3], [4, 5, 6]]])
     data = iquib(arr)
-    result = np.sum(data, axis=axis, **kwargs)
+    result = func(**{data_kwarg: data}, **kwargs)
     children = {idx: result[idx] for idx in np.ndindex(result.get_shape().get_value())}
 
     values = {idx: child.get_value() for idx, child in children.items()}
@@ -23,5 +15,26 @@ def test_axiswise_invalidation(axis, indices_to_invalidate, expected_invalidated
 
     invalidated_result_indices = {idx for idx, child in children.items() if not child.is_cache_valid}
     new_values = {idx: child.get_value() for idx, child in children.items()}
-    changed_result_indices = {idx for idx in new_values if values[idx] != new_values[idx]}
-    assert invalidated_result_indices == expected_invalidated_result_indices == changed_result_indices
+    changed_result_indices = {idx for idx in new_values if not np.array_equal(values[idx], new_values[idx])}
+    assert invalidated_result_indices == changed_result_indices
+
+
+@mark.parametrize('indices_to_invalidate', [-1, 0, (0, 0), (0, 1, 2), ...])
+@mark.parametrize('axis', [-1, (-1, 1), 0, 1, 2, (0, 2), (0, 1), None])
+@mark.parametrize('keepdims', [True, False, None])
+@mark.parametrize('where', [True, False, [[[True], [False]]], None])
+def test_axiswise_invalidation_with_sum(indices_to_invalidate, axis, keepdims, where):
+    kwargs = dict(axis=axis)
+    if keepdims is not None:
+        kwargs['keepdims'] = keepdims
+    if where is not None:
+        kwargs['where'] = where
+    check_invalidation(np.sum, kwargs, 'a', indices_to_invalidate)
+
+
+@mark.parametrize('indices_to_invalidate', [-1, 0, (0, 0), (0, 1, 2), ...])
+@mark.parametrize('axis', [0, 1, 2, -1, -2])
+@mark.parametrize('func_out_dims', [0, 1, 2])
+def test_axiswise_invalidation_with_apply_along_axis(indices_to_invalidate, axis, func_out_dims):
+    func1d = lambda slice: np.sum(slice).reshape((1,) * func_out_dims)
+    check_invalidation(np.apply_along_axis, dict(func1d=func1d, axis=axis), 'arr', indices_to_invalidate)
