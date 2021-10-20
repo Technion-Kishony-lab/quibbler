bf9d86a84bfbc41bf1ce7ec15efe09a604d07ccd
Maor Kleinberger <kmaork@gmail.com>
2021-10-20 14:19:56 +0300

    Improved axiswise test

--- tests/unit/quib/graphics/test_axiswise_graphics_function_quib.py
+++ tests/unit/quib/graphics/test_axiswise_graphics_function_quib.py
@@ -3,8 +3,6 @@ from pytest import mark
 
 from pyquibbler import iquib
 
-from ..utils import PathBuilder
-
 
 @mark.parametrize(['axis', 'indices_to_invalidate', 'expected_invalidated_result_indices', 'kwargs'], [
     (0, (0, 1, 2), {(1, 2,)}, dict()),
@@ -14,18 +12,16 @@ from ..utils import PathBuilder
     (None, 0, {(0, 0, 0)}, dict(keepdims=True)),
     (2, (0, 1), set(), dict(where=[[[True], [False]]])),
 ])
-@mark.parametrize('arr', [
-    [[[1, 2, 3], [4, 5, 6]]],
-    np.array([[[1, 2, 3], [4, 5, 6]]])
-])
-def test_axiswise_invalidation(axis, indices_to_invalidate, expected_invalidated_result_indices, arr, kwargs):
+def test_axiswise_invalidation(axis, indices_to_invalidate, expected_invalidated_result_indices, kwargs):
+    arr = np.array([[[1, 2, 3], [4, 5, 6]]])
     data = iquib(arr)
     result = np.sum(data, axis=axis, **kwargs)
     children = {idx: result[idx] for idx in np.ndindex(result.get_shape().get_value())}
-    for child in children.values():
-        child.get_value()
 
-    data.invalidate_and_redraw_at_path(PathBuilder(data)[indices_to_invalidate].path)
+    values = {idx: child.get_value() for idx, child in children.items()}
+    data[indices_to_invalidate] = 999
 
     invalidated_result_indices = {idx for idx, child in children.items() if not child.is_cache_valid}
-    assert invalidated_result_indices == expected_invalidated_result_indices
+    new_values = {idx: child.get_value() for idx, child in children.items()}
+    changed_result_indices = {idx for idx in new_values if values[idx] != new_values[idx]}
+    assert invalidated_result_indices == expected_invalidated_result_indices == changed_result_indices
