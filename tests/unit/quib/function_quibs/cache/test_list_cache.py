22b06e166efc5eecc5b72493e0a607c37f1d3b42
Maor <maor.kern@gmail.com>
2021-10-20 11:05:42 +0300

    Add more list cache tests

--- tests/unit/quib/function_quibs/cache/test_list_cache.py
+++ tests/unit/quib/function_quibs/cache/test_list_cache.py
@@ -8,6 +8,16 @@ def list_cache():
     return ListShallowCache.create_from_result([1, 2, 3])
 
 
+def assert_uncached_paths_popped_out_falses_in_list(uncached_paths, lst):
+    for path in uncached_paths:
+        assert len(path) == 1
+        component = path[0]
+        assert lst[component.component] is False
+        lst[component.component] = True
+
+    assert all([o is True for o in lst])
+
+
 def test_list_cache_matches_result_of_list_type_and_same_length(list_cache):
     assert list_cache.matches_result([1, 2, 3])
 
@@ -19,3 +29,15 @@ def test_list_cache_does_not_match_result_of_list_type_and_different_length(list
 def test_list_cache_does_not_match_result_of_non_list_type(list_cache):
     assert not list_cache.matches_result({1, 2, 3})
 
+
+def test_list_cache_starts_invalid(list_cache):
+    uncached_paths = list_cache.get_uncached_paths([])
+
+    assert_uncached_paths_popped_out_falses_in_list(uncached_paths, [False, False, False])
+
+
+def test_list_cache_set_valid_makes_uncached_paths_empty(list_cache):
+    list_cache.set_valid_value_at_path([], [2, 3, 4])
+    uncached_paths = list_cache.get_uncached_paths([])
+
+    assert len(uncached_paths) == 0
