c76da854c9c0cbf35c42d98648595fc2dca0414e
Maor <maor.kern@gmail.com>
2021-10-20 10:57:56 +0300

    Finish matching results tests

--- tests/unit/quib/function_quibs/cache/test_list_cache.py
+++ tests/unit/quib/function_quibs/cache/test_list_cache.py
@@ -10,3 +10,12 @@ def list_cache():
 
 def test_list_cache_matches_result_of_list_type_and_same_length(list_cache):
     assert list_cache.matches_result([1, 2, 3])
+
+
+def test_list_cache_does_not_match_result_of_list_type_and_different_length(list_cache):
+    assert not list_cache.matches_result([1, 2, 3, 4])
+
+
+def test_list_cache_does_not_match_result_of_non_list_type(list_cache):
+    assert not list_cache.matches_result({1, 2, 3})
+
