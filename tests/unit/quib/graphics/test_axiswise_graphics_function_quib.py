a46524520f4b50d4c66f89183c2a74146661bac0
Maor Kleinberger <kmaork@gmail.com>
2021-10-22 15:26:52 +0300

    Improved tests

--- tests/unit/quib/graphics/test_axiswise_graphics_function_quib.py
+++ tests/unit/quib/graphics/test_axiswise_graphics_function_quib.py
@@ -4,20 +4,19 @@ from pytest import mark
 from pyquibbler import iquib
 
 
-def check_invalidation(func, indices_to_invalidate):
+def check_invalidation(func, data, indices_to_invalidate):
     """
     Run func on an ndarray iquib, change the iquib in the given indices,
     and verify that the invalidated indices were also the ones that changed values.
     Make sure that func works in a way that guarantees that when a value in the input changes,
     all affected values in the result also change.
     """
-    arr = np.array([[[1, 2, 3], [4, 5, 6]]])
-    data = iquib(arr)
-    result = func(data)
+    input_quib = iquib(data)
+    result = func(input_quib)
     children = {idx: result[idx] for idx in np.ndindex(result.get_shape().get_value())}
 
     values = {idx: child.get_value() for idx, child in children.items()}
-    data[indices_to_invalidate] = 999
+    input_quib[indices_to_invalidate] = 999
 
     invalidated_result_indices = {idx for idx, child in children.items() if not child.is_cache_valid}
     new_values = {idx: child.get_value() for idx, child in children.items()}
@@ -25,27 +24,35 @@ def check_invalidation(func, indices_to_invalidate):
     assert invalidated_result_indices == changed_result_indices
 
 
-@mark.parametrize('indices_to_invalidate', [-1, 0, (0, 0), (0, 1, 2), ...])
+# A 3d array in which every dimension has a different size
+parametrize_data = mark.parametrize('data', [np.arange(24).reshape((2, 3, 4))])
+parametrize_indices = mark.parametrize('indices_to_invalidate', [-1, 0, (0, 0), (0, 1, 2), (0, ...), [True, False]])
+
+
+@parametrize_indices
+@parametrize_data
 @mark.parametrize('axis', [-1, (-1, 1), 0, 1, 2, (0, 2), (0, 1), None])
 @mark.parametrize('keepdims', [True, False, None])
-@mark.parametrize('where', [True, False, [[[True], [False]]], None])
-def test_axiswise_invalidation_with_sum(indices_to_invalidate, axis, keepdims, where):
+@mark.parametrize('where', [True, False, [[[True], [False], [True]]], None])
+def test_axiswise_invalidation_with_sum(indices_to_invalidate, axis, keepdims, where, data):
     kwargs = dict(axis=axis)
     if keepdims is not None:
         kwargs['keepdims'] = keepdims
     if where is not None:
         kwargs['where'] = where
-    check_invalidation(lambda data: np.sum(data, **kwargs), indices_to_invalidate)
+    check_invalidation(lambda iq: np.sum(iq, **kwargs), data, indices_to_invalidate)
 
 
-@mark.parametrize('indices_to_invalidate', [-1, 0, (0, 0), (0, 1, 2), ...])
+@parametrize_indices
+@parametrize_data
 @mark.parametrize('axis', [0, 1, 2, -1, -2])
 @mark.parametrize('func_out_dims', [0, 1, 2])
-def test_axiswise_invalidation_with_apply_along_axis(indices_to_invalidate, axis, func_out_dims):
+def test_axiswise_invalidation_with_apply_along_axis(indices_to_invalidate, axis, func_out_dims, data):
     func1d = lambda slice: np.sum(slice).reshape((1,) * func_out_dims)
-    check_invalidation(lambda data: np.apply_along_axis(func1d, axis, data), indices_to_invalidate)
+    check_invalidation(lambda iq: np.apply_along_axis(func1d, axis, iq), data, indices_to_invalidate)
 
 
-@mark.parametrize('indices_to_invalidate', [-1, 0, (0, 0), (0, 1, 2), ...])
-def test_axiswise_invalidation_with_vectorize(indices_to_invalidate):
-    check_invalidation(lambda data: np.vectorize(lambda x: x)(data), indices_to_invalidate)
+@parametrize_indices
+@parametrize_data
+def test_axiswise_invalidation_with_vectorize(indices_to_invalidate, data):
+    check_invalidation(lambda iq: np.vectorize(lambda x: x)(iq), data, indices_to_invalidate)
