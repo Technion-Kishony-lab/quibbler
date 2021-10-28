fc8b600fcae4a1fe0a4bc26a40176b61dc363c08
Maor <maor.kern@gmail.com>
2021-10-28 10:01:24 +0300

    Move tests from integration to functional

--- tests/functional/quib/graphics/test_graphics_function_quib.py
+++ tests/functional/quib/graphics/test_graphics_function_quib.py
@@ -70,3 +70,37 @@ def test_graphics_function_quib_rerun_removes_artists_created(monkeypatch, mock_
 
     assert len(mock_artists_collector.all_mock_artists_created) == 2
     assert len(mock_axes.artists) == 1
+
+
+@pytest.mark.regression
+def test_graphics_function_quib_doesnt_fail_on_removal_of_artists(axes):
+    input_quib = iquib([1, 2, 3])
+    axes.plot(input_quib)
+    axes.cla()
+
+    input_quib[0] = 10
+
+
+def test_graphics_function_quib_copy_color(axes):
+    input_quib = iquib([1., 2., 3.])
+    plot_quib = axes.plot(input_quib)
+
+    artist_color_upon_creation = plot_quib.get_value()[0].get_color()
+    input_quib.invalidate_and_redraw_at_path()
+    artist_color_after_redraw = plot_quib.get_value()[0].get_color()
+
+    assert artist_color_upon_creation == artist_color_after_redraw
+
+
+def test_graphics_function_quib_does_not_copy_color(axes):
+    input_quib = iquib([1., 2., 3.])
+    color_quib = iquib([1, 0, 0])
+    plot_quib = axes.plot(input_quib, color=color_quib)
+
+    artist_color_upon_creation = plot_quib.get_value()[0].get_color()
+    color_quib[1] = 1
+    artist_color_after_color_change = plot_quib.get_value()[0].get_color()
+
+    assert artist_color_upon_creation == [1, 0, 0]
+    assert artist_color_after_color_change == [1, 1, 0]
+
