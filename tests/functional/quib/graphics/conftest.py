568e24b55930423e5ce6c044665fc7223bbfa4a5
Maor Kleinberger <kmaork@gmail.com>
2021-11-01 16:53:40 +0200

    Moved graphics fixtures to conftest

---
+++ tests/functional/quib/graphics/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from unittest import mock
+
+from pyquibbler.quib.graphics import global_collecting
+
+
+class MockArtistsCollector:
+    def __init__(self, mock_axes):
+        self.mock_axes = mock_axes
+        self.all_mock_artists_created = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def _create_mock_artist(self):
+        artist = mock.Mock()
+        artist.axes = self.mock_axes
+        self.mock_axes.artists.append(artist)
+        return artist
+
+    @property
+    def artists_collected(self):
+        artist = self._create_mock_artist()
+        self.all_mock_artists_created.append(artist)
+        return [artist]
+
+
+@pytest.fixture()
+def mock_axes():
+    axes = mock.Mock()
+    axes.artists = []
+    return axes
+
+
+@pytest.fixture()
+def mock_artists_collector(mock_axes, monkeypatch):
+    collector = MockArtistsCollector(mock_axes)
+    monkeypatch.setattr(global_collecting, "ArtistsCollector", lambda: collector)
+    return collector
--- tests/functional/quib/graphics/test_graphics_function_quib.py
+++ tests/functional/quib/graphics/test_graphics_function_quib.py
@@ -4,38 +4,7 @@ from unittest import mock
 
 from pyquibbler import iquib
 from pyquibbler.quib.assignment.assignment import PathComponent
-from pyquibbler.quib.graphics import GraphicsFunctionQuib, global_collecting
-
-
-class MockArtistsCollector:
-    def __init__(self, mock_axes):
-        self.mock_axes = mock_axes
-        self.all_mock_artists_created = []
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
-    def _create_mock_artist(self):
-        artist = mock.Mock()
-        artist.axes = self.mock_axes
-        self.mock_axes.artists.append(artist)
-        return artist
-
-    @property
-    def artists_collected(self):
-        artist = self._create_mock_artist()
-        self.all_mock_artists_created.append(artist)
-        return [artist]
-
-
-@pytest.fixture()
-def mock_axes():
-    axes = mock.Mock()
-    axes.artists = []
-    return axes
+from pyquibbler.quib.graphics import GraphicsFunctionQuib
 
 
 def get_graphics_quib(func):
@@ -57,10 +26,7 @@ def test_graphics_function_quib_get_value_returns_value():
     assert res == mock_func.return_value
 
 
-def test_graphics_function_quib_rerun_removes_artists_created(monkeypatch, mock_axes):
-    mock_artists_collector = MockArtistsCollector(mock_axes)
-    monkeypatch.setattr(global_collecting, "ArtistsCollector", lambda: mock_artists_collector)
-
+def test_graphics_function_quib_rerun_removes_artists_created(monkeypatch, mock_axes, mock_artists_collector):
     father_quib = iquib(1)
     quib = get_graphics_quib(mock.Mock())
     father_quib.add_child(quib)
@@ -103,4 +69,3 @@ def test_graphics_function_quib_does_not_copy_color(axes):
 
     assert artist_color_upon_creation == [1, 0, 0]
     assert artist_color_after_color_change == [1, 1, 0]
-
