047315626eaee82570e50a24e4bdaed6a7efe6dc
Maor <maor.kern@gmail.com>
2021-11-10 14:36:52 +0200

    Add to __init__

--- pyquibbler/__init__.py
+++ pyquibbler/__init__.py
@@ -2,5 +2,5 @@ from .decorators import quibbler_user_function
 from .performance_utils import timer, get_timer
 from .quib import iquib, CacheBehavior
 from .third_party_overriding import override_all
-from .user_utils import q, q_graphics
+from .user_utils import q, q_graphics, reset_impure_function_quibs
 from .quib.assignment import Assignment
