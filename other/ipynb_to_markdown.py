b4cf7ba4d8330f58606418f78ce5bbaf0ba15ffb
Roy Kishony <rkishony@technion.ac.il>
2021-11-20 12:29:13 +0200

    refine ipynb_to_markdown

--- other/ipynb_to_markdown.py
+++ other/ipynb_to_markdown.py
@@ -2,9 +2,21 @@ import os
 import glob
 
 notebooks_path = '../examples/notebooks'
-wiki_path = '../../pyquibbler.wiki/files'
+wiki_examples_path = '../../pyquibbler.wiki/examples'
+wiki_gif_path = '../../pyquibbler.wiki/images/demo_gif'
 
-files = glob.glob(notebooks_path + '/*')
-print(files)
+os.system("jupyter nbconvert --to markdown " + notebooks_path + "/quibdemo*.ipynb")
+os.system("mv " + notebooks_path + "/quibdemo*.md " + wiki_examples_path)
 
-os.system("jupyter nbconvert --to markdown ../examples/notebooks/*.ipynb")
+full_files = glob.glob(wiki_examples_path + '/*.md')
+print(full_files)
+
+for full_file in full_files:
+    _, file = os.path.split(full_file)
+    file = file[:-3]
+    if os.path.isfile(wiki_gif_path + '/' + file + ".gif"):
+        with open(full_file, "a") as myfile:
+            print(file)
+            myfile.write("[[/images/demo_gif/" + file + ".gif]]")
+            myfile.write("")
+
