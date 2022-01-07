0e4625d2262d210fd9245cc77679d4dd0c536aed
Maor <maor.kern@gmail.com>
2022-01-07 02:11:40 -0500

    WIP

--- pyquibbler/refactor/utilities/general_utils.py
+++ pyquibbler/refactor/utilities/general_utils.py
@@ -14,7 +14,8 @@ def create_empty_array_with_values_at_indices(shape: tuple, indices: Any, value:
     Create an empty array in a given shape with `values` at `indices`. All other indices will be filled with
     `empty_value`
     """
-    res = np.zeros(shape, dtype=dtype or np.array(value).dtype)
+    from pyquibbler.refactor.quib.factory import get_original_func
+    res = get_original_func(np.zeros)(shape, dtype=dtype or get_original_func(np.array)(value).dtype)
     if empty_value is not None:
         res.fill(empty_value)
 
