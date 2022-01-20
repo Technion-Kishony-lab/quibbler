77abdaec1f8e29722b0ca49d45f736c871e2d900
Maor <maor.kern@gmail.com>
2022-01-20 08:10:36 +0200

    Fix issue with duplicate user utils

--- pyquibbler/__init__.py
+++ pyquibbler/__init__.py
@@ -1,8 +1,7 @@
 from .decorators import quibbler_user_function
-from .user_utils import reset_impure_function_quibs, save_quibs, set_project_path, load_quibs, undo, \
-    redo, has_redos, has_undos, redraw_central_refresh_graphics_function_quibs
 from pyquibbler.assignment import Assignment
 from pyquibbler.quib import CacheBehavior, iquib
 from .function_overriding import override_all
 from .utilities.performance_utils import timer
-from .utilities.user_utils import q
+from .utilities.user_utils import q, reset_impure_function_quibs, save_quibs, set_project_path, load_quibs, undo, \
+    redo, has_redos, has_undos, redraw_central_refresh_graphics_function_quibs
