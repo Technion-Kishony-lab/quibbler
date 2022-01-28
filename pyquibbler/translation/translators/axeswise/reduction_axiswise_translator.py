9ca3603a2faa266f574bb6033769784639b8f290
rkishony <rkishony@technion.ac.il>
2022-01-28 19:09:21 +0200

    use original np.logical_ functions to prevent collide with overrides

--- pyquibbler/translation/translators/axeswise/reduction_axiswise_translator.py
+++ pyquibbler/translation/translators/axeswise/reduction_axiswise_translator.py
@@ -16,7 +16,8 @@ class ReductionAxiswiseForwardsPathTranslator(AxiswiseForwardsPathTranslator):
         Calculate forward index translation for reduction functions by reducing the boolean arrays
         with the same reduction params.
         """
-        return np.logical_or.reduce(boolean_mask, **args_dict)
+        from pyquibbler.utilities.numpy_original_functions import np_logical_or
+        return np_logical_or.reduce(boolean_mask, **args_dict)
 
 
 class ReductionAxiswiseBackwardsPathTranslator(AxiswiseBackwardsPathTranslator):
@@ -24,10 +25,11 @@ class ReductionAxiswiseBackwardsPathTranslator(AxiswiseBackwardsPathTranslator):
     TRANSLATION_RELATED_ARGS = REDUCTION_ARGS
 
     def _backwards_translate_bool_mask(self, args_dict, source: Source, bool_mask: np.ndarray) -> np.ndarray:
+        from pyquibbler.utilities.numpy_original_functions import np_logical_and
         keepdims = args_dict['keepdims']
         if not keepdims:
             input_core_dims = args_dict['axis']
             if input_core_dims is not None:
                 bool_mask = np.expand_dims(bool_mask, input_core_dims)
         bool_mask = np.broadcast_to(bool_mask, np.shape(source.value))
-        return np.logical_and(bool_mask, args_dict['where'])
+        return np_logical_and(bool_mask, args_dict['where'])
