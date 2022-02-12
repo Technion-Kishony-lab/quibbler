19cc4ab05966a9b9f29dc86e21fd7a1d3f72efae
Roy Kishony <rkishony@technion.ac.il>
2022-02-12 17:47:22 +0200

    take care of list-extension assignment - add failed test

--- tests/functional/quib/test_quib/invalidation/test_shape_only_data_arg.py
+++ tests/functional/quib/test_quib/invalidation/test_shape_only_data_arg.py
@@ -21,3 +21,21 @@ def test_shape_only_function_quib_is_fully_invalidated_upon_change_in_data_sourc
 
     a.assign_value(np.arange(6).reshape(3, 2))
     assert b.cache_status == CacheStatus.ALL_INVALID
+
+
+def test_shape_only_function_quib_is_fully_invalidated_upon_list_extension_assignment():
+    a = iquib([1, 2, 3])
+    b = np.zeros_like(a).setp(cache_behavior='on')
+    b.get_value()
+
+    a[:] = [11, 12, 13, 14]  # list is extended
+    assert b.cache_status == CacheStatus.ALL_INVALID
+
+
+def test_shape_only_function_quib_is_not_invalidated_upon_list_assignment():
+    a = iquib([1, 2, 3])
+    b = np.zeros_like(a).setp(cache_behavior='on')
+    b.get_value()
+
+    a[1] = 100
+    assert b.cache_status == CacheStatus.ALL_VALID
