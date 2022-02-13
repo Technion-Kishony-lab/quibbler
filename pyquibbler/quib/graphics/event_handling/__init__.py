4dd2970ca3f2b5703a26e3975b840942c798e104
Roy Kishony <rkishony@technion.ac.il>
2022-02-13 02:16:32 +0200

    add inverse_assigner for set_xlim, set_ylim

--- pyquibbler/quib/graphics/event_handling/__init__.py
+++ pyquibbler/quib/graphics/event_handling/__init__.py
@@ -1,2 +1,3 @@
 from .plot_inverse_assigner import get_override_group_for_axes_plot
+from .set_lim_inverse_assigner import get_override_group_for_axes_set_lim
 from .canvas_event_handler import CanvasEventHandler
--- pyquibbler/quib/graphics/event_handling/graphics_inverse_assigner.py
+++ pyquibbler/quib/graphics/event_handling/graphics_inverse_assigner.py
@@ -40,3 +40,23 @@ def inverse_assign_drawing_func(drawing_func: Callable,
         else:
             override_group.apply()
             return override_group
+
+
+def inverse_assign_axes_lim_func(drawing_func: Callable,
+                                 args: Iterable[Any],
+                                 lim: Tuple[float, float],
+                                 is_override_removal: bool,
+                                 ):
+    """
+    Reverse a graphics set axis lim quib, assigning to it's argument values based on change in axes
+    """
+
+    inverse_assigner_func = GRAPHICS_REVERSE_ASSIGNERS.get(drawing_func.__qualname__)
+    if inverse_assigner_func is not None:
+        try:
+            override_group = inverse_assigner_func(args=args, lim=lim, is_override_removal=is_override_removal)
+        except AssignmentCancelledByUserException:
+            pass
+        else:
+            override_group.apply()
+            return override_group
