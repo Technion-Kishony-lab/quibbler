63ad42ab6d7b4ef311a0792e7ee18f8e034d4a87
Roy Kishony <rkishony@technion.ac.il>
2022-02-18 14:19:15 +0200

    add failed test

--- tests/functional/quib/test_quib/invalidation/test_shape_only_data_arg.py
+++ tests/functional/quib/test_quib/invalidation/test_shape_only_data_arg.py
@@ -1,40 +1,50 @@
 import numpy as np
+import pytest
 
-from pyquibbler import iquib
+from pyquibbler import iquib, q
 from pyquibbler.cache.cache import CacheStatus
 from tests.functional.utils import PathBuilder
 
+SHAPE_ONLY_FUNCTIONS_TO_TEST = [
+    np.zeros_like,
+    np.shape,
+    len,
+]
 
-def test_shape_only_function_quib_is_not_invalidated_upon_change_in_data_source_elements():
+@pytest.mark.parametrize("func", SHAPE_ONLY_FUNCTIONS_TO_TEST)
+def test_shape_only_function_quib_is_not_invalidated_upon_change_in_data_source_elements(func):
     a = iquib(np.arange(6).reshape(2, 3))
-    b = np.zeros_like(a).setp(cache_behavior='on')
+    b = q(func, a).setp(cache_behavior='on')
     b.get_value()
 
     a[:] = 7
     assert b.cache_status == CacheStatus.ALL_VALID
 
 
-def test_shape_only_function_quib_is_fully_invalidated_upon_change_in_data_source_shape():
+@pytest.mark.parametrize("func", SHAPE_ONLY_FUNCTIONS_TO_TEST)
+def test_shape_only_function_quib_is_fully_invalidated_upon_change_in_data_source_shape(func):
     a = iquib(np.arange(6).reshape(2, 3))
-    b = np.zeros_like(a).setp(cache_behavior='on')
+    b = q(func, a).setp(cache_behavior='on')
     b.get_value()
 
     a.assign_value(np.arange(6).reshape(3, 2))
     assert b.cache_status == CacheStatus.ALL_INVALID
 
 
-def test_shape_only_function_quib_is_fully_invalidated_upon_list_extension_assignment():
+@pytest.mark.parametrize("func", SHAPE_ONLY_FUNCTIONS_TO_TEST)
+def test_shape_only_function_quib_is_fully_invalidated_upon_list_extension_assignment(func):
     a = iquib([1, 2, 3])
-    b = np.zeros_like(a).setp(cache_behavior='on')
+    b = q(func, a).setp(cache_behavior='on')
     b.get_value()
 
     a[:] = [11, 12, 13, 14]  # list is extended
     assert b.cache_status == CacheStatus.ALL_INVALID
 
 
-def test_shape_only_function_quib_is_not_invalidated_upon_list_assignment():
+@pytest.mark.parametrize("func", SHAPE_ONLY_FUNCTIONS_TO_TEST)
+def test_shape_only_function_quib_is_not_invalidated_upon_list_assignment(func):
     a = iquib([1, 2, 3])
-    b = np.zeros_like(a).setp(cache_behavior='on')
+    b = q(func, a).setp(cache_behavior='on')
     b.get_value()
 
     a[1] = 100
