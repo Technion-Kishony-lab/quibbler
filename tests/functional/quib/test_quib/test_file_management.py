bcd16a99d10db5731bd30dde72d3628b3bfbca37
Roy Kishony <rkishony@technion.ac.il>
2022-02-23 23:11:19 +0200

    adjust tests

--- tests/functional/quib/test_quib/test_file_management.py
+++ tests/functional/quib/test_quib/test_file_management.py
@@ -1,15 +1,11 @@
+import pytest
+
+from pyquibbler.quib.save_assignments import FileNotDefinedException, ResponseToFileNotDefined
 
 
-def test_multiple_quib_save_without_given_name(create_quib_with_return_value):
+def test_quib_wont_save_without_assigned_name(create_quib_with_return_value):
     example_quib = create_quib_with_return_value(5, allow_overriding=True)
     example_quib.assign(10)
-    another_quib = create_quib_with_return_value(6).setp(allow_overriding=True)
-    another_quib.assign(8)
 
-    example_quib.save()
-    another_quib.save()
-    another_quib.load()
-    example_quib.load()
-
-    assert example_quib.get_value() == 10
-    assert another_quib.get_value() == 8
+    with pytest.raises(FileNotDefinedException):
+        example_quib.save(ResponseToFileNotDefined.RAISE)
