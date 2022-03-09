a5650d02b76a2b040a6a0daa3d93c9691ae8768a
Roy Kishony <rkishony@technion.ac.il>
2022-03-09 09:07:17 +0200

    flake8

--- pyquibbler/assignment/default_value.py
+++ pyquibbler/assignment/default_value.py
@@ -1,10 +1,10 @@
 class Singleton(object):
     _instance = None
 
-    def __new__(class_, *args, **kwargs):
-        if not isinstance(class_._instance, class_):
-            class_._instance = object.__new__(class_, *args, **kwargs)
-        return class_._instance
+    def __new__(cls, *args, **kwargs):
+        if not isinstance(cls._instance, cls):
+            cls._instance = object.__new__(cls, *args, **kwargs)
+        return cls._instance
 
 
 class Default(Singleton):
