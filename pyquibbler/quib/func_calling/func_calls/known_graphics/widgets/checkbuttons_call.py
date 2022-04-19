5463f2e346780236be220b815e76dbb3da08774c
Roy Kishony <rkishony@technion.ac.il>
2022-04-20 02:33:58 +0300

    fix checkbuttons_call - all tests pass

--- pyquibbler/quib/func_calling/func_calls/known_graphics/widgets/checkbuttons_call.py
+++ pyquibbler/quib/func_calling/func_calls/known_graphics/widgets/checkbuttons_call.py
@@ -11,11 +11,14 @@ class CheckButtonsQuibFuncCall(WidgetQuibFuncCall):
     def on_change_checkbuttons(self, widget, new_value):
         from pyquibbler.quib.quib import Quib
         actives = self.func_args_kwargs.get('actives')
+        buttons_checked = widget.get_status()
+        labels = self.func_args_kwargs.get('labels')
+        new_value_index = labels.index(new_value)
         if isinstance(actives, Quib):
-            buttons_checked = widget.get_status()
-            labels = self.func_args_kwargs.get('labels')
-            new_value_index = labels.index(new_value)
             self._inverse_assign(actives, [PathComponent(indexed_cls=list, component=new_value_index)],
+                                 value=buttons_checked[new_value_index])
+        elif isinstance(actives[new_value_index], Quib):
+            self._inverse_assign(actives[new_value_index], [],
                                  value=buttons_checked[new_value_index])
 
     def _connect_callbacks(self, widget: CheckButtons):
