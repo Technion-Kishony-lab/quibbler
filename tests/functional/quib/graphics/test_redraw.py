eed192745ac460b3c80832e684fa4cb3c18a0ad9
Roy Kishony <rkishony@technion.ac.il>
2022-04-22 14:30:39 +0300

    add tests

--- tests/functional/quib/graphics/test_redraw.py
+++ tests/functional/quib/graphics/test_redraw.py
@@ -1,5 +1,9 @@
+import matplotlib.pyplot as plt
+
 from unittest import mock
 from unittest.mock import Mock
+
+import pytest
 
 from pyquibbler import iquib
 from pyquibbler.function_definitions import add_definition_for_function
@@ -8,18 +12,39 @@ from pyquibbler.quib.factory import create_quib
 from pyquibbler.quib.graphics.redraw import aggregate_redraw_mode, redraw_axeses
 
 
-def test_redraw_axes_happy_flow(mock_axes):
-    redraw_axeses({mock_axes})
 
-    mock_axes.figure.canvas.draw.assert_called_once()
+@pytest.fixture
+def figure() -> plt.Figure:
+    plt.close("all")
+    figure = plt.figure()
+    figure.canvas = Mock()
+    return figure
+
+
+@pytest.fixture
+def axes1(figure) -> plt.Axes:
+    axes1 = figure.add_axes([0, 0, 0.5, 1])
+    return axes1
+
+
+@pytest.fixture
+def axes2(figure) -> plt.Axes:
+    axes2 = figure.add_axes([0.5, 0, 0.5, 1])
+    return axes2
+
+
+def test_redraw_axes_happy_flow(figure, axes1):
+    redraw_axeses({axes1})
+
+    figure.canvas.draw.assert_called_once()
 
 
 def test_redraw_in_aggregate_mode():
     mock_func = mock.Mock()
     quib = iquib(1)
     add_definition_for_function(func=mock_func, func_definition=create_func_definition(is_graphics=True,
-                                                                                           lazy=True,
-                                                                                           replace_previous_quibs_on_artists=True))
+                                                                                       lazy=True,
+                                                                                       replace_previous_quibs_on_artists=True))
     _ = create_quib(func=mock_func, args=(quib,))
     assert mock_func.call_count == 0, "sanity"
 
@@ -31,13 +56,14 @@ def test_redraw_in_aggregate_mode():
     assert mock_func.call_count == 1
 
 
-def test_redraw_axeses_does_not_redraw_same_canvas_twice():
-    mock_axes1 = Mock()
-    mock_axes2 = Mock()
-    mock_canvas = Mock()
-    mock_axes1.figure.canvas = mock_canvas
-    mock_axes2.figure.canvas = mock_canvas
+def test_redraw_axeses_does_not_redraw_same_canvas_twice(figure, axes1, axes2):
+    redraw_axeses({axes1, axes2})
 
-    redraw_axeses({mock_axes1, mock_axes2})
+    figure.canvas.draw.assert_called_once()
 
-    mock_canvas.draw.assert_called_once()
+
+def test_redraw_after_figure_closed(figure, axes1):
+    plt.close(figure)
+    redraw_axeses({axes1})
+    figure.canvas.draw.assert_not_called()
+
