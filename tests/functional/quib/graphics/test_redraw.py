ac844c9578552d6483165ff67d8673355813bd10
Roy Kishony <rkishony@technion.ac.il>
2022-04-24 07:51:26 +0300

    failed tests: graphics persistence

--- tests/functional/quib/graphics/test_redraw.py
+++ tests/functional/quib/graphics/test_redraw.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from unittest import mock
 from unittest.mock import Mock
 
+import numpy as np
 import pytest
 
 from pyquibbler import iquib
@@ -10,7 +11,7 @@ from pyquibbler.function_definitions import add_definition_for_function
 from pyquibbler.function_definitions.func_definition import create_func_definition
 from pyquibbler.quib.factory import create_quib
 from pyquibbler.quib.graphics.redraw import aggregate_redraw_mode, redraw_axeses
-
+from weakref import ref
 
 
 @pytest.fixture
@@ -62,8 +63,45 @@ def test_redraw_axeses_does_not_redraw_same_canvas_twice(figure, axes1, axes2):
     figure.canvas.draw.assert_called_once()
 
 
+# To prevent pyimageXX bug in TK on notebook. see issue: #119
+@pytest.mark.regression
 def test_redraw_after_figure_closed(figure, axes1):
     plt.close(figure)
     redraw_axeses({axes1})
     figure.canvas.draw.assert_not_called()
 
+
+def test_quibs_deleted_after_figure_closed(figure, axes1):
+    a = iquib(np.array([1, 2]))
+    b = axes1.plot(a, picker=True)
+    ref_a = ref(a)
+    ref_b = ref(b)
+    del a, b
+    plt.close(figure)
+
+    assert ref_a() is None
+    assert ref_b() is None
+
+
+def test_quibs_deleted_after_figure_clf(figure, axes1):
+    a = iquib(np.array([1, 2]))
+    b = axes1.plot(a, picker=True)
+    ref_a = ref(a)
+    ref_b = ref(b)
+    del a, b
+    figure.clf()
+
+    assert ref_a() is None
+    assert ref_b() is None
+
+
+def test_quibs_deleted_after_axes_cla(figure, axes1):
+    a = iquib(np.array([1, 2]))
+    b = axes1.plot(a, picker=True)
+    ref_a = ref(a)
+    ref_b = ref(b)
+    del a, b
+    axes1.cla()
+
+    assert ref_a() is None
+    assert ref_b() is None
