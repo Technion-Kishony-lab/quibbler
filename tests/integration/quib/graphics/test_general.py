b0f30746560a6a53b324bd75708bd1360c0f0fd7
Roy Kishony <rkishony@technion.ac.il>
2022-04-26 23:11:30 +0300

    add speed benchmarks

--- tests/integration/quib/graphics/test_general.py
+++ tests/integration/quib/graphics/test_general.py
@@ -7,7 +7,6 @@ from pyquibbler.graphics.global_collecting import AxesCreatedDuringQuibEvaluatio
 
 @pytest.mark.regression
 def test_graphics_function_quib_doesnt_fail_on_removal_of_artists(axes):
-    override_all()
     input_quib = iquib([1, 2, 3])
     plt.plot(input_quib)
     plt.cla()
