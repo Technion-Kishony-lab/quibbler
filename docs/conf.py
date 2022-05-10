9ab49253a9bf30daf4d0235d5f6d1c9fc3e92abc
Roy Kishony <rkishony@technion.ac.il>
2022-05-10 10:00:49 +0300

    include custom.css

--- docs/conf.py
+++ docs/conf.py
@@ -57,9 +57,15 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+
+html_static_path = ['_static']
 
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 
 autodoc_member_order = 'bysource'
+
+# html_style = 'custom.css'
+
+def setup(app):
+    app.add_css_file('custom.css')
