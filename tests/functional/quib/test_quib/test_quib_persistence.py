f3bf3c8d535975249da0556cc2aa28b8808d5e4f
Roy Kishony <rkishony@technion.ac.il>
2022-06-28 18:08:11 +0300

    add failed tests

--- tests/functional/quib/test_quib/test_quib_persistence.py
+++ tests/functional/quib/test_quib/test_quib_persistence.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 from unittest import mock
 from unittest.mock import Mock
@@ -22,3 +23,33 @@ def test_quibs_deleted_when_variable_clears():
 
     assert ref_a() is None
     assert ref_b() is None
+
+
+def test_plot_quibs_persist_on_artist(axes):
+    a = iquib(1)
+    plot_quib = axes.plot(1, a)
+    ref_plot_quib = ref(plot_quib)
+
+    del plot_quib
+    assert ref_plot_quib() is not None
+
+
+def test_vectorize_plot_quibs_persist_on_artist(axes):
+    a = iquib([1, 2])
+    plot_quib = None
+
+    def plot_point(x):
+        nonlocal plot_quib
+        plot_quib = axes.plot(x, x)
+
+    vectorize_plot_point = np.vectorize(plot_point, signature='()->()', pass_quibs=True, is_graphics=True)
+    vectorize_plot_quib = vectorize_plot_point(a)
+
+    ref_plot_quib = ref(plot_quib)
+    ref_vectorize_plot_quib = ref(vectorize_plot_quib)
+
+    del plot_quib
+    del vectorize_plot_quib
+
+    assert ref_plot_quib() is not None
+    assert ref_vectorize_plot_quib() is not None
