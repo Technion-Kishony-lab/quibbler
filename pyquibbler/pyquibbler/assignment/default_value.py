eee4bcf2c7a988925b5b67dd39ff529e0b6db00c
Roy Kishony <rkishony@technion.ac.il>
2022-07-07 03:13:22 +0300

    docs for default value

--- pyquibbler/pyquibbler/assignment/default_value.py
+++ pyquibbler/pyquibbler/assignment/default_value.py
@@ -9,21 +9,29 @@ class Singleton(object):
 
 class Default(Singleton):
     """
-    A default value for remove-overriding assignments.
+    A default value for reverting overriding assignments.
 
-    `default` is a singleton instance which is used as a dummy value in quib assignments to indicate
-    removal of overriding assignments.
-
-    `default` allows removing quib assignments by assigning it as value in quib-assignment syntax.
+    `default` is a singleton instance which is used as value in quib assignments to indicate
+    reseting the quib's value back to its default functional value (namely, overruling prior overriding assignments).
 
     Examples
     --------
-
-    >>> quib[2:5] = default
-
-    >>> quib.assign(default)
-
-    >>> quib['year'] = default
+    >>> n = iquib(7)
+    >>> x = np.arange(n).setp(allow_overriding=True)
+    >>> x.get_value()
+    array([ 0, 1, 2, 3, 4, 5,  6])
+    >>>
+    >>> x[1:-1] = 100  # override functional values
+    >>> x.get_value()
+    array([ 0, 100, 100, 100, 100, 100,  6])
+    >>>
+    >>> x[2:-2] = default  # set specified elements back to default
+    >>> x.get_value()
+    array([ 0, 100, 2, 3, 4, 100,  6])
+    >>>
+    >>> x.assign(default)  # reset all the array back to default
+    >>> x.get_value()
+    array([ 0, 1, 2, 3, 4, 5,  6])
     """
 
     def __repr__(self):
