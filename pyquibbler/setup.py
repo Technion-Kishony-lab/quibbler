959a08c92001c6f3f4e603147a41511ad158aa0c
Maor <maor.kern@gmail.com>
2022-07-09 19:42:43 +0300

    Bump version

--- pyquibbler/setup.py
+++ pyquibbler/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyquibbler",
-    version='0.1.0',
+    version='0.1.1',
     python_requires='>=3.8',
     long_description="Quibbler",
     packages=find_packages(),
