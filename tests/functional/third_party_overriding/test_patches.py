dadd81bfe6b74aefb05201a5033d3ba419f6c222
Roy Kishony <rkishony@technion.ac.il>
2022-07-24 01:36:00 +0300

    add tests

--- tests/functional/third_party_overriding/test_patches.py
+++ tests/functional/third_party_overriding/test_patches.py
@@ -1,27 +1,46 @@
-from
+import pytest
 
-
-def test_axes_add_patch_on_non_quib(axes):
-    axes.
-
-
-from pyquibbler import iquib, initialize_quibbler
-initialize_quibbler()
-
-import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.patches import Rectangle
+from pyquibbler import iquib
 
-import matplotlib
-matplotlib.use("TkAgg")
 
-fig = plt.figure()
-ax = plt.gca()
-ax.axis([0, 10, 0, 10])
-w = iquib(2)
-# w = 2
-r1 = Rectangle((1, 1), w, 6)
-ax.add_patch(r1)
+def check_rectangle_location(rectangle: Rectangle, args):
+    xy, w, h = args
+    return np.array_equal(rectangle.get_xy(), xy) \
+        and rectangle.get_width() == w \
+        and rectangle.get_height() == h
 
-w.assign(3)
 
-plt.show()
+@pytest.fixture()
+def get_only_patch_from_axes(axes):
+
+    def get_():
+        assert len(axes.patches) == 1
+        return axes.patches[0]
+
+    return get_
+
+
+def test_axes_add_patch_on_non_quib(axes, get_only_patch_from_axes):
+    p = Rectangle((1, 2), 3, 4)
+    axes.add_patch(p)
+    assert p is get_only_patch_from_axes()
+
+
+def test_axes_add_patch_on_quib(axes, get_only_patch_from_axes):
+    w = iquib(3)
+    p = Rectangle((1, 2), w, 4)
+    axes.add_patch(p)
+    assert p.get_value() is get_only_patch_from_axes()
+
+
+def test_patch_update(axes, get_only_patch_from_axes):
+    w = iquib(3)
+    p = Rectangle((1, 2), w, 4)
+    axes.add_patch(p)
+    assert check_rectangle_location(p.get_value(), ((1, 2), 3, 4)), "sanity"
+
+    w.assign(10)
+    assert p.get_value() is get_only_patch_from_axes()
+    assert check_rectangle_location(p.get_value(), ((1, 2), 10, 4))
