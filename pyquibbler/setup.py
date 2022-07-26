8da95a4bdd580af1eea6f30e482f439db9a18ce0
Maor <maor.kern@gmail.com>
2022-07-26 17:59:49 +0300

    Add hard link to README.md

--- pyquibbler/setup.py
+++ pyquibbler/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 setup_directory = Path(__file__).parent
-long_description = (setup_directory / ".." / "README.md").read_text()
+long_description = (setup_directory / "README.md").read_text()
 
 setup(
     name="pyquibbler",
