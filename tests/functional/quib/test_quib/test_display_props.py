e1c96d04e0f318b42c42bd78e1f147326a5e0214
Roy Kishony <rkishony@technion.ac.il>
2022-07-30 17:01:19 +0300

    add test for coverage

--- tests/functional/quib/test_quib/test_display_props.py
+++ tests/functional/quib/test_quib/test_display_props.py
@@ -8,3 +8,11 @@ def test_display_props_of_fquib():
     a = iquib(0)
     b = a + 1
     b.display_props()
+
+
+def test_wip():
+    a = iquib(1)
+    b = a + 2
+    a.named_children
+    b.named_parents
+    
