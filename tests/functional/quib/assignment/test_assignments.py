9fb6d7f3914491b7f72c78b03354225ca84a7a3a
Roy Kishony <rkishony@technion.ac.il>
2022-08-02 16:38:11 +0300

    add test (pass)

--- tests/functional/quib/assignment/test_assignments.py
+++ tests/functional/quib/assignment/test_assignments.py
@@ -1,4 +1,9 @@
-from pyquibbler import iquib
+import numpy as np
+import pytest
+
+from pyquibbler import iquib, Quib
+from pyquibbler.assignment.assignment import AssignmentWithTolerance
+from pyquibbler.path import PathComponent
 
 
 def test_assignment_do_not_change_each_other():
@@ -9,3 +14,17 @@ def test_assignment_do_not_change_each_other():
     a.get_value()
 
     assert a.handler.overrider.get(()).value == [10, 20, 30]
+
+
+def test_assignment_with_tolerance():
+    a = iquib(np.array([1., 2.]))
+    b: Quib = a * 100
+
+    assignment = AssignmentWithTolerance.from_value_path_tolerance(
+        value=123.456789,
+        path=[PathComponent(np.ndarray, 0)],
+        tolerance=5.)
+
+    b.handler.apply_assignment(assignment)
+
+    assert a.handler.overrider.get(assignment.remove_class_from_path()).value == 1.23
