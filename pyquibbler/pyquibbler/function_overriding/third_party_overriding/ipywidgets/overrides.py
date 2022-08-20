f7ec073b9b2cd58c0bda821694d994a8af91acfe
Roy Kishony <rkishony@technion.ac.il>
2022-08-20 19:25:35 +0300

    add Widget -> Quib connection. All works fine

--- pyquibbler/pyquibbler/function_overriding/third_party_overriding/ipywidgets/overrides.py
+++ pyquibbler/pyquibbler/function_overriding/third_party_overriding/ipywidgets/overrides.py
@@ -1,5 +1,10 @@
 import functools
+from typing import Dict
+
 from pyquibbler.quib.quib import Quib
+
+
+TRAIT_TO_QUIB_ATTR = '_quibbler_trait_to_quib'
 
 
 def is_ipywidgets_installed() -> bool:
@@ -14,6 +19,13 @@ def is_ipywidgets_installed() -> bool:
     return True
 
 
+def _get_or_create_trait_to_quib(widget) -> Dict[str, Quib]:
+    if not hasattr(widget, TRAIT_TO_QUIB_ATTR):
+        setattr(widget, TRAIT_TO_QUIB_ATTR, {})
+
+    return getattr(widget, TRAIT_TO_QUIB_ATTR)
+
+
 def get_wrapper_for_trait_type_set():
 
     # noinspection PyPackageRequirements
@@ -24,13 +36,25 @@ def get_wrapper_for_trait_type_set():
     @functools.wraps(func)
     def _set(self, obj, value):
         if isinstance(value, Quib):
+            quib = value
 
             def on_quib_change(new_value):
                 func(self, obj, new_value)
 
-            value.add_callback(on_quib_change)
-            value = value.get_value()
+            # indicate on the widget that the trait is connected to the quib:
+            _get_or_create_trait_to_quib(obj)[self.name] = quib
 
+            # set the quib to call back when changed:
+            quib.add_callback(on_quib_change)
+
+            value = quib.get_value()
+
+        else:
+            quib = _get_or_create_trait_to_quib(obj).get(self.name, None)
+            if quib is not None:
+                quib.assign(value)
+                return
+            
         func(self, obj, value)
 
     return _set
