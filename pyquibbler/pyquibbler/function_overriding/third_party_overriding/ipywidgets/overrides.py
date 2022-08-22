693aaba4c0329f8d67571d9ddb5b13652bd51382
Roy Kishony <rkishony@technion.ac.il>
2022-08-22 09:21:13 +0300

    organize into separate funcs

--- pyquibbler/pyquibbler/function_overriding/third_party_overriding/ipywidgets/overrides.py
+++ pyquibbler/pyquibbler/function_overriding/third_party_overriding/ipywidgets/overrides.py
@@ -29,55 +29,50 @@ def get_widgets_requiring_tolerance_assignment():
     }
 
 
-def _get_or_create_trait_to_quib(widget) -> Dict[str, Quib]:
-    if not hasattr(widget, TRAIT_TO_QUIB_ATTR):
-        setattr(widget, TRAIT_TO_QUIB_ATTR, {})
-
-    return getattr(widget, TRAIT_TO_QUIB_ATTR)
+def _actual_set(widget, trait, value):
+    """ The snippet from TraitType.set the does the bare set of the trait"""
+    widget._trait_values[trait] = value
 
 
-# TODO: Implement push to undo/redo on mouse drop.
-#  possibly using:
-#  w = ipyevents.Event(source=widget, watched_events=['mouseup'])
-#  w.on_dom_event(on_drop)
+def set_widget_to_update_quib_upon_change(widget, trait, quib):
+    # TODO: Implement push to undo/redo on mouse drop.
+    #  We will need for this to get mouse release from the widget, possibly using:
+    #  w = ipyevents.Event(source=widget, watched_events=['mouseup'])
+    #  w.on_dom_event(on_drop)
+
+    def on_widget_change(change):
+        new_value = change['new']
+        quib.assign(new_value)
+
+    widget.observe(on_widget_change, trait)
+
+
+def set_quib_to_update_widget_upon_change(widget, trait, quib):
+
+    def on_quib_change(new_value):
+        _actual_set(widget, trait, new_value)
+
+    quib.add_callback(on_quib_change)
+
 
 def get_wrapper_for_trait_type_set():
 
     # noinspection PyPackageRequirements
     from traitlets import TraitType
+    original_set = TraitType.set
 
-    def actual_set(self, obj, value):
-        new_value = self._validate(obj, value)
-        obj._trait_values[self.name] = new_value
-
-    func = TraitType.set
-
-    @functools.wraps(func)
-    def _set(self, obj, value):
+    @functools.wraps(original_set)
+    def quiby_set(self, obj, value):
         if isinstance(value, Quib):
-            quib = value
+            # We cannot call original_set because it buffers future update to the quib.
+            _actual_set(obj, self.name, value.get_value())
 
-            def on_quib_change(new_value):
-                func(self, obj, new_value)
-
-            # indicate on the widget that the trait is connected to the quib:
-            _get_or_create_trait_to_quib(obj)[self.name] = quib
-
-            # set the quib to call back when changed:
-            quib.add_callback(on_quib_change)
-
-            value = quib.get_value()
-            actual_set(self, obj, value)
-            return
+            set_widget_to_update_quib_upon_change(obj, self.name, value)
+            set_quib_to_update_widget_upon_change(obj, self.name, value)
         else:
-            quib = _get_or_create_trait_to_quib(obj).get(self.name, None)
-            if quib is not None:
-                quib.assign(value)
-                return
-            
-        func(self, obj, value)
+            original_set(self, obj, value)
 
-    return _set
+    return quiby_set
 
 
 def override_ipywidgets_if_installed():
