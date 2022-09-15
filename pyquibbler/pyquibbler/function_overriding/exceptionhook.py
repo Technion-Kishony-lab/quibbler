8cfd926a559fbe6f7a9800fdaad65b58481a3e51
Roy Kishony <rkishony@technion.ac.il>
2022-09-16 01:26:50 +0300

    add simple exceptionhook

---
+++ pyquibbler/pyquibbler/function_overriding/exceptionhook.py
@@ -0,0 +1,18 @@
+import sys
+import logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(stream=sys.stdout)
+logger.addHandler(handler)
+
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+
+    logger.error("Uncaught exception", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+def override_excepthook():
+    sys.excepthook = handle_exception
