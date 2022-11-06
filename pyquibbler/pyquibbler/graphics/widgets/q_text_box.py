7e825f8825f601cd14b6f532cddf4091d52dda9b
Roy Kishony <rkishony@technion.ac.il>
2022-11-06 15:53:40 +0200

    fix bug in matplotlib TextBox (update in macOSX backend)

--- pyquibbler/pyquibbler/graphics/widgets/q_text_box.py
+++ pyquibbler/pyquibbler/graphics/widgets/q_text_box.py
@@ -1,5 +1,7 @@
 from matplotlib.widgets import TextBox
 
+from pyquibbler.quib.graphics.redraw import redraw_canvas
+
 
 class QTextBox(TextBox):
     """
@@ -21,3 +23,8 @@ class QTextBox(TextBox):
 
     def on_submit(self, func):
         super().on_submit(lambda text: func(self.convert_text_to_original_type(text)))
+
+    def _rendercursor(self):
+        # Matplotlib has a bug - does not update on MacOSX. To fix, we run our own redraw_canvas:
+        super()._rendercursor()
+        redraw_canvas(self.ax.figure.canvas)
