fff9d1838f96c49b4533d8aec8bb35d7306e6808
Roy Kishony <rkishony@technion.ac.il>
2022-11-11 19:05:43 +0200

    add additional quiby ndarray attributes

--- pyquibbler/pyquibbler/function_overriding/third_party_overriding/numpy/quiby_attributes.py
+++ pyquibbler/pyquibbler/function_overriding/third_party_overriding/numpy/quiby_attributes.py
@@ -7,6 +7,11 @@ from pyquibbler.function_definitions.func_definition import FuncDefinition
 
 ATTRIBUTES_TO_FUNCS = {
     'T': np.transpose,
+    'imag': np.imag,
+    'ndim': np.ndim,
+    'real': np.real,
+    'shape': np.shape,
+    'size': np.size,
 }
 
 
