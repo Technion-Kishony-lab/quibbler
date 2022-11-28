748affa2fc2b9225240eaa47bba1cbbe1d3dacca
Roy Kishony <rkishony@technion.ac.il>
2022-11-28 08:42:14 +0200

    add failed test: drag along zero slope

--- tests/integration/quib/graphics/test_graphics_assignment_x_y_conflict.py
+++ tests/integration/quib/graphics/test_graphics_assignment_x_y_conflict.py
@@ -15,3 +15,16 @@ def test_drag_along_shallow_slope(create_axes_mouse_press_move_release_events, a
 
     create_axes_mouse_press_move_release_events(((0, 0), (0.5, 0)))
     assert marker_x.get_value() == 0.496
+
+
+def test_drag_along_zero_slope(create_axes_mouse_press_move_release_events, axes):
+
+    axes.set_xlim(-1, 1)
+    axes.set_ylim(-1, 1)
+
+    marker_x = iquib(0.)
+    marker_y = 0 * marker_x  # line with shallow slope
+    plot_quib = axes.plot(marker_x, marker_y, marker='o')
+
+    create_axes_mouse_press_move_release_events(((0, 0), (0.5, 0)))
+    assert marker_x.get_value() == 0.5
