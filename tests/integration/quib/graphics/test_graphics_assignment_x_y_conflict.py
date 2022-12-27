0b8f2e3fa48b4915c41855067264385d4f00ac9d
Roy Kishony <rkishony@technion.ac.il>
2022-12-27 10:05:02 +0200

    overshoot correction
    enable overshoot
    
    
    os
    
    
    sel wow

--- tests/integration/quib/graphics/test_graphics_assignment_x_y_conflict.py
+++ tests/integration/quib/graphics/test_graphics_assignment_x_y_conflict.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import pytest
 
 from pyquibbler import iquib, undo, redo
 from tests.integration.quib.graphics.widgets.utils import count_canvas_draws, count_redraws
@@ -11,7 +12,7 @@ def test_drag_along_shallow_slope(create_axes_mouse_press_move_release_events, a
 
     marker_x = iquib(0.)
     marker_y = 0.1 * marker_x  # line with shallow slope
-    plot_quib = axes.plot(marker_x, marker_y, marker='o')
+    axes.plot(marker_x, marker_y, marker='o')
 
     create_axes_mouse_press_move_release_events(((0, 0), (0.5, 0)))
     assert marker_x.get_value() == 0.496
@@ -23,10 +24,21 @@ def test_drag_along_zero_slope(create_axes_mouse_press_move_release_events, axes
     axes.set_ylim(-1, 1)
 
     marker_x = iquib(0.)
-    marker_y = 0 * marker_x  # line with shallow slope
-    plot_quib = axes.plot(marker_x, marker_y, marker='o')
+    marker_y = 0 * marker_x  # line with zero slope
+    axes.plot(marker_x, marker_y, marker='o')
 
     create_axes_mouse_press_move_release_events(((0, 0), (0.5, 0)))
     assert marker_x.get_value() == 0.498
 
 
+def test_drag_same_arg_binary_operator(create_axes_mouse_press_move_release_events, axes):
+
+    axes.set_xlim(-1, 4)
+    axes.set_ylim(-1, 4)
+
+    x = iquib(1.)
+    xx = x + x
+    axes.plot(xx, xx, marker='o')
+
+    create_axes_mouse_press_move_release_events(((2, 2), (3, 3)))
+    assert abs(xx.get_value() - 3) < 0.01
