bf9df200ef3b2969f82ce09c109df202371f906a
Roy Kishony <rkishony@technion.ac.il>
2022-12-29 17:08:47 +0200

    failed test: exception on mouse drop

--- tests/functional/quib/test_quib/test_exception_causing_assignments.py
+++ tests/functional/quib/test_quib/test_exception_causing_assignments.py
@@ -1,18 +1,36 @@
 from pyquibbler import iquib, quiby
 from pyquibbler.quib.graphics.graphics_assignment_mode import graphics_assignment_mode
-
-
-@quiby(is_graphics=True)
-def validate_input(x):
-    if x < 0:
-        raise Exception("x cannot be negative")
+from pyquibbler.quib.graphics.redraw import start_dragging, end_dragging
 
 
 def test_prevent_assignments_causing_exception():
+    @quiby(is_graphics=True)
+    def validate_input(x):
+        if x < 0:
+            raise Exception("x cannot be negative")
 
     a = iquib(1)
     validate = validate_input(a)
 
     with graphics_assignment_mode(True):
         a.assign(-1)
+    assert a.get_value() == 1
+
+
+def test_prevent_assignments_causing_exception_on_drop():
+    @quiby(is_graphics=True)
+    def validate_input(x):
+        if x < 0:
+            raise Exception("x cannot be negative")
+
+    a = iquib(1)
+    validate = validate_input(a)
+    validate.graphics_update = 'drop'
+
+    start_dragging()
+    with graphics_assignment_mode(True):
+        a.assign(-1)
+        assert a.get_value() == -1
+
+    end_dragging()
     assert a.get_value() == 1
