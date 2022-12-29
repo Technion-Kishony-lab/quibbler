1a25b99b48286fb13eda0636520f8efca67d4852
Roy Kishony <rkishony@technion.ac.il>
2022-12-30 00:22:29 +0200

    offset drag for scatter. all tests pass

--- pyquibbler/pyquibbler/quib/graphics/event_handling/enhance_pick_event.py
+++ pyquibbler/pyquibbler/quib/graphics/event_handling/enhance_pick_event.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib.backend_bases import PickEvent
+from matplotlib.collections import PathCollection
 from matplotlib.lines import Line2D
 
 
@@ -11,19 +12,27 @@ class ZeroDistance:
         return self
 
 
-def _enhance_line2D_pick_event(pick_event: PickEvent):
-
+def _get_line2D_xy_data(pick_event: PickEvent):
+    """
+    Get the distance from the mouse to picked vertices of Line2D Artist
+    """
     # TODO: may need to distinguish between segment picking and point picking.
     #  See the `contains` method of Line2D
     artist: Line2D = pick_event.artist
-    xy_data = artist.get_xydata()
-    ind = pick_event.ind
-    mouseevent = pick_event.mouseevent
-    pick_event.dxy = xy_data[ind, :] - [[mouseevent.xdata, mouseevent.ydata]]
+    return artist.get_xydata()
 
 
-ARTIST_TYPES_TO_REDUCE_FUNCTIONS = {
-    Line2D: _enhance_line2D_pick_event,
+def _get_PathCollection_xy_data(pick_event: PickEvent):
+    """
+    Get the distance from the mouse to picked vertices of PathCollection Artist (acreated by plt.scatter)
+    """
+    artist: PathCollection = pick_event.artist
+    return artist.get_offsets().data
+
+
+ARTIST_TYPES_TO_GET_XY_DATA = {
+    Line2D: _get_line2D_xy_data,
+    PathCollection: _get_PathCollection_xy_data,
 }
 
 
@@ -31,12 +40,15 @@ def _enhance_pick_event(pick_event: PickEvent):
     """
     Store distance from mouse to picked points
     """
-    reduce_function = ARTIST_TYPES_TO_REDUCE_FUNCTIONS.get(type(pick_event.artist), None)
-    if reduce_function is None:
+    get_xy_data = ARTIST_TYPES_TO_GET_XY_DATA.get(type(pick_event.artist), None)
+    if get_xy_data is None:
         try:
             ind = pick_event.ind
             pick_event.dxy = np.zeros(len(ind), 2)
         except (AttributeError, TypeError):
             pick_event.dxy = ZeroDistance()
     else:
-        reduce_function(pick_event)
+        xy_data = get_xy_data(pick_event)
+        ind = pick_event.ind
+        mouseevent = pick_event.mouseevent
+        pick_event.dxy = xy_data[ind, :] - [[mouseevent.xdata, mouseevent.ydata]]
