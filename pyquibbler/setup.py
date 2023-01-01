24e4d94274a308506efde0869bf6c1f952df2aca
Roy Kishony <rkishony@technion.ac.il>
2023-01-01 10:42:12 +0200

    bump version 0.2.4

--- pyquibbler/setup.py
+++ pyquibbler/setup.py
@@ -20,7 +20,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     name="pyquibbler",
-    version='0.2.3',
+    version='0.2.4',
     python_requires='>=3.8',
     long_description=long_description,
     long_description_content_type='text/markdown',
