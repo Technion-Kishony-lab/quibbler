fafa6b5cfb22930bf1a79734148595bcd1db1571
rkishony <rkishony@technion.ac.il>
2024-05-21 21:49:35 -0400

    bump version 0.2.9

--- pyquibbler/setup.py
+++ pyquibbler/setup.py
@@ -24,7 +24,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     name="pyquibbler",
-    version='0.2.8',
+    version='0.2.9',
     python_requires='>=3.8',
     long_description=long_description,
     long_description_content_type='text/markdown',
