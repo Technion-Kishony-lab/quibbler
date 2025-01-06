2a538cdb95803eb66d7aa6a9ac49459a3093ab06
Roy Kishony <rkishony@technion.ac.il>
2025-01-06 19:01:10 +0200

    solve_single_point_with_two_variables WIP
    solv

--- pyquibbler/pyquibbler/quib/graphics/event_handling/solvers.py
+++ pyquibbler/pyquibbler/quib/graphics/event_handling/solvers.py
@@ -17,7 +17,7 @@ def solve_single_point_on_curve(func: Callable, v0, v1, xy: PointArray, toleranc
     Parameters
     ----------
     func : Callable
-        A function of a single variable. Returns a PointXY.
+        A function of a single variable. Returns a PointArray.
     v0 : float
         The initial guess for the variable value.
     v1 : float
@@ -82,4 +82,64 @@ def test_solve_single_point_on_curve(func, v0, v1, xy, expected_v, accuracy, exp
     result, point, num_iter = solve_single_point_on_curve(func, v0, v1, xy)
     assert abs(result - expected_v) <= accuracy, f'{name}: result is {result}, expected {expected_v}'
     assert num_iter == expected_nun_iter, f'{name}: num_iter is {num_iter}, expected {expected_nun_iter}'
+
+
+def solve_single_point_with_two_variables(func: Callable, v0, v1, w0, w1, xy: PointArray, tolerance: float = 1,
+                                          max_iter: int = 10,
+                                          p0: Optional[PointArray] = None, p1: Optional[PointArray] = None
+                                          ) -> (float, float, PointArray, int):
+    """
+    Find the variable values for which the curve defined by func is closest to the point xy.
+
+    Parameters
+    ----------
+    func : Callable
+        A function of two variables. Returns a PointArray.
+    v0 : float
+        The initial guess for the first variable value.
+    v1 : float
+        The second guess for the first variable value.
+    w0 : float
+        The initial guess for the second variable value.
+    w1 : float
+        The second guess for the second variable value.
+    xy : PointArray
+        The point for which the curve should be closest to.
+    tolerance : float
+        The tolerance for the distance between the curve and the point.
+    p0 : Optional[PointArray]
+        The point on the curve at v0, w0. If None, it is calculated by calling func(v0, w0).
+    p1 : Optional[PointArray]
+        The point on the curve at v1, w1. If None, it is calculated by calling func(v1, w1).
+    """
+
+    if p0 is None:
+        p0 = func(v0, w0)
+    if p1 is None:
+        p1 = func(v1, w1)
+
+    # by convention, v0 affects x (and maybe y) and w0 affects y (and maybe x)
+    if func(v0, w0).x == func(v1, w0).x and func(v0, w0).y == func(v0, w1).y:
+        return v1, w1, p1, 0
+
+    # check if each variable affects only one coordinate
+    # if so, we can solve the two variables independently using solve_single_point_on_curve
+    if func(v0, w0).x == func(v1, w0).x and func(v0, w0).y == func(v0, w1).y:
+        v, p_v, num_iter_v = solve_single_point_on_curve(lambda x: func(x, w0), v0, v1, xy)
+        w, p_w, num_iter_w = solve_single_point_on_curve(lambda y: func(v, y), w0, w1, xy)
+        p = func(v, w)
+        return v, w, p, num_iter_v + num_iter_w
+
+
+    num_iter = 0
+    closest_value = None
+    closest_distance = None
+    closest_point = None
+
+    # this time we are not constrained to a single curve. playing with the two variables can bring us to any point
+    # therefore get_closest_point_on_line is not used
+    # use standard min finder:
+    from scipy.optimize import minimize
+    def distance(vw):
+        return norm(func(vw[0], vw[1]) - xy)
 
