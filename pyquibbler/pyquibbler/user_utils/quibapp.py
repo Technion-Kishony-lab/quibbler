412b7400ec98140182cebdf234e36473920c71f9
rkishony <rkishony@technion.ac.il>
2025-04-05 21:47:22 +0300

    refine quibapp

--- pyquibbler/pyquibbler/user_utils/quibapp.py
+++ pyquibbler/pyquibbler/user_utils/quibapp.py
@@ -2,14 +2,12 @@ from __future__ import annotations
 
 import matplotlib.pyplot as plt
 from typing import Optional, Dict
-
 from matplotlib.figure import Figure
+from matplotlib.widgets import Button
+from functools import partial
 
 from pyquibbler.project.project import Project, NothingToUndoException, NothingToRedoException, \
     NoProjectDirectoryException
-from matplotlib.widgets import Button, TextBox
-from pyquibbler.utilities.input_validation_utils import InvalidArgumentException
-from functools import partial
 
 APP_TITLE = 'Data Quibbler'
 
@@ -23,105 +21,125 @@ def figure_without_toolbar(*args, **kwargs):
     return fig
 
 
-def truncated_str(s: str, n: Optional[int] = None):
-    return s if n is None or len(s) < n else '...' + s[-(n - 3):]
+class ColorButton(Button):
+    # Class-level color variables.
+    active_face_color = '#bbbbbb'
+    inactive_face_color = '#ffffff'
+    active_text_color = '#000000'
+    inactive_text_color = '#888888'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._active = True
+
+    def set_active(self, active: bool):
+        self._active = active
+        super().set_active(active)
+        # Set face and text colors based on active state.
+        self.ax.set_facecolor(
+            self.active_face_color if active else self.inactive_face_color
+        )
+        self.label.set_color(
+            self.active_text_color if active else self.inactive_text_color
+        )
+        self.ax.figure.canvas.draw_idle()
+
+    def draw(self, renderer):
+        self.label.set_color(
+            self.active_text_color if self._active else self.inactive_text_color
+        )
+        super().draw(renderer)
 
 
 class QuibApp:
-    """
-    An app to control quibbler project functionalities such as save/load undo/redo
-    actions on many quibs
-    """
-
     current_quibapp: Optional[QuibApp] = None
-    current_project: Optional[Project] = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.current_quibapp is None:
+            cls.current_quibapp = super(QuibApp, cls).__new__(cls)
+        return cls.current_quibapp
 
     def __init__(self):
+        # Prevent reinitialization on subsequent instantiation attempts.
+        if hasattr(self, "_initialized") and self._initialized:
+            return
+        self._initialized = True
+
         self.figure: Optional[Figure] = None
-        self._buttons: Optional[Dict[str, Button]] = None
-        self._path_text = None
+        self._buttons: Dict[str, Button] = {}
+        self.project = Project.get_or_create()
+
+        self.create_app_figure()
+
+    @classmethod
+    def get_or_create(cls) -> QuibApp:
+        """
+        Get the current instance of QuibApp or create a new one if it doesn't exist.
+        """
+        if cls.current_quibapp is None:
+            cls.current_quibapp = cls()
+        return cls.current_quibapp
 
     def create_app_figure(self):
-        fig = figure_without_toolbar(num=APP_TITLE, figsize=(3.3, 1))
+        fig = figure_without_toolbar(num=APP_TITLE, figsize=(3.3, 0.5))
         fig.canvas.mpl_connect('close_event', self._on_figure_close)
         self.figure = fig
 
-        h = 0.20  # button height
-        w = 0.14  # Button width
-        mh = 0.05  # left/right margin
-        mv = 0.15  # distance from bottom/top
+        # Layout parameters
+        h = 0.46  # button height
+        w = 0.14  # button width
+        mh = 0.05  # margin (left/right)
+        mv = 0.2  # margin (bottom/top)
         spc = 0.03  # space between buttons
-        mt = 0.22  # top margin
 
-        # buttons:
-        self._buttons = dict()
-        self._buttons['undo'] = Button(ax=fig.add_axes([mh, mv, w, h]), label='Undo')
-        self._buttons['redo'] = Button(ax=fig.add_axes([mh + w + spc, mv, w, h]), label='Redo')
-        self._buttons['save'] = Button(ax=fig.add_axes([1 - mh - w - 2 * (spc + w), mv, w, h]), label='Save')
-        self._buttons['load'] = Button(ax=fig.add_axes([1 - mh - w - 1 * (spc + w), mv, w, h]), label='Load')
-        self._buttons['sync'] = Button(ax=fig.add_axes([1 - mh - w - 0 * (spc + w), mv, w, h]), label='Sync')
+        # Create buttons and assign to the buttons dict
+        self._buttons['undo'] = ColorButton(ax=fig.add_axes([mh, mv, w, h]), label='Undo')
+        self._buttons['redo'] = ColorButton(ax=fig.add_axes([mh + w + spc, mv, w, h]), label='Redo')
+        self._buttons['save'] = ColorButton(ax=fig.add_axes([1 - mh - w - 2 * (spc + w), mv, w, h]), label='Save')
+        self._buttons['load'] = ColorButton(ax=fig.add_axes([1 - mh - w - 1 * (spc + w), mv, w, h]), label='Load')
+        self._buttons['sync'] = ColorButton(ax=fig.add_axes([1 - mh - w, mv, w, h]), label='Sync')
 
-        project = self.current_project
-        buttons_funcs_exceptions = (
-            ('undo', project.undo, NothingToUndoException),
-            ('redo', project.redo, NothingToRedoException),
-            ('save', project.save_quibs, NoProjectDirectoryException),
-            ('load', project.load_quibs, NoProjectDirectoryException),
-            ('sync', project.sync_quibs, NoProjectDirectoryException),
-        )
-        for button, func, exception in buttons_funcs_exceptions:
-            call_func = partial(self.on_button_click, project_func=func, exception=exception)
-            self._buttons[button].on_clicked(call_func)
+        # Map button actions to project functions and handle expected exceptions
+        button_actions = [
+            ('undo', self.project.undo, NothingToUndoException),
+            ('redo', self.project.redo, NothingToRedoException),
+            ('save', self.project.save_quibs, NoProjectDirectoryException),
+            ('load', self.project.load_quibs, NoProjectDirectoryException),
+            ('sync', self.project.sync_quibs, NoProjectDirectoryException),
+        ]
 
-        # path:
-        ax = fig.add_axes([mh, 1 - mt - h, 1 - 2 * mh, h])
-        self._path_text = TextBox(ax=ax, label='', initial=self._get_project_path_str())
-        self._path_text.active = False
-        self._path_text.on_submit(self._set_path)
-        self.current_project.on_path_change = self._handle_path_change
-        ax.text(0, 1.1, 'path:', va='bottom', ha='left')
+        for key, func, exception in button_actions:
+            callback = partial(self.on_button_click, project_func=func, exception=exception)
+            self._buttons[key].on_clicked(callback)
+            self._buttons[key].set_active(True)
 
-    def _get_project_path_str(self):
-        return '' if self.current_project.directory is None else str(self.current_project.directory)
+        self._update_undo_redo_enabled()
+        self.project.add_undo_redo_callback(self._update_undo_redo_enabled)
 
-    def _set_path(self, path):
-        try:
-            self.current_project.path = path
-        except InvalidArgumentException:
-            pass
+    def _on_figure_close(self, event):
+        self.close()
 
-    def _handle_path_change(self, path):
-        self._path_text.set_val(truncated_str(self._get_project_path_str(), 38))
-
-    @classmethod
-    def get_or_create(cls):
-        if cls.current_quibapp is None:
-            cls.current_project = Project.get_or_create()
-            cls.current_quibapp = cls()
-            cls.current_quibapp.create_app_figure()
-        return cls.current_quibapp
-
-    @classmethod
-    def close(cls):
-        current_quibapp = cls.current_quibapp
-        if current_quibapp is not None:
-            plt.close(current_quibapp.figure)
-            cls.current_project = None
-            cls.current_quibapp = None
-
-    @classmethod
-    def _on_figure_close(cls, *args) -> None:
-        cls.current_quibapp = None
-        cls.current_project.on_path_change = None
-        cls.current_project = None
-
-    @classmethod
-    def on_button_click(cls, *args, project_func, exception) -> bool:
+    def on_button_click(self, event, project_func, exception):
         try:
             project_func()
         except exception as e:
-            cls._display_message(str(e))
+            self._display_message(str(e))
 
-    @classmethod
-    def _display_message(cls, msg):
+    def _display_message(self, msg: str):
         print(msg)
+
+    def _update_undo_redo_enabled(self):
+        if not self._buttons:
+            return
+        self._buttons['undo'].set_active(self.project.can_undo())
+        self._buttons['redo'].set_active(self.project.can_redo())
+
+    def close(self):
+        if self.figure:
+            plt.close(self.figure)
+        self.figure = None
+        if self.current_quibapp is self:
+            self.current_quibapp = None
+            self.project.remove_undo_redo_callback(self._update_undo_redo_enabled)
+
+            type(self).current_quibapp = None
