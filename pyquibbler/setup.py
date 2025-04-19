fb371d899b4aa1e56fa8c2074962177607ff5af6
rkishony <rkishony@technion.ac.il>
2025-04-20 00:21:50 +0300

    ver 1.0.1

--- pyquibbler/setup.py
+++ pyquibbler/setup.py
@@ -24,7 +24,7 @@ setup(
         'Operating System :: OS Independent',
     ],
     name="pyquibbler",
-    version='1.0.0',
+    version='1.0.1',
     python_requires='>=3.10',
     long_description=long_description,
     long_description_content_type='text/markdown',
